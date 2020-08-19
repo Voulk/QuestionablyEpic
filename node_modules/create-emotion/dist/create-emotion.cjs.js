c69b4525c9511c360a70df5d9b0cd4c90cdd52e4
Ptolemae <62045440+Ptolemae@users.noreply.github.com>
2020-08-19 21:25:14 +1000

    I Give Up :despair:

--- node_modules/create-emotion/dist/create-emotion.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./create-emotion.cjs.prod.js");
-} else {
-  module.exports = require("./create-emotion.cjs.dev.js");
-}
--- node_modules/react-loading-overlay/.storybook/config.js
+++
@@ -1,7 +0,0 @@
-import { configure } from '@storybook/react';
-
-function loadStories() {
-  require('./stories/LoadingOverlay');
-}
-
-configure(loadStories, module);
