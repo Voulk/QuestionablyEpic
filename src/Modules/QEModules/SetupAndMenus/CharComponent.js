86c02b61abe32e664db56e08ae0f62048f2e0ee8
VoulkInf3 <itsvoulk@gmail.com>
2020-09-03 16:37:37 -0500

    Clean up

--- src/Modules/QEModules/SetupAndMenus/CharComponent.js
+++ src/Modules/QEModules/SetupAndMenus/CharComponent.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
-import druid from "../../../Images/DruidSmall.png";
-
+const specImages = {
+    'Restoration Druid' : require('../../../Images/DruidSmall.png')
+   
+}
 
 export default function CharComponent(props) {
     
         return(
             <div className="charComp">
                 <div className="charLeft" >
-                    <img src={druid} />
+                    <img src={specImages[props.spec]} />
                 </div>
 
                 <div className="charRight">
                     <p style={{fontSize: "18px", marginTop: "4px", marginBottom: "0px", textAlign: "left", fontWeight: "bold"}}>{props.name}</p>
                     <p style={{fontSize: "14px", marginTop: "2px", textAlign: "left"}}>{props.spec}</p>
 
-
                 </div>
-                   
-
-
+            
             </div>
-
-
-
         )
 }
