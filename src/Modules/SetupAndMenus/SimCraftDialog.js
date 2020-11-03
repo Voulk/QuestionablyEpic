314885bb5ffe7dfe8e4bc3277d3ba48b358cfc79
Ptolemae <62045440+Ptolemae@users.noreply.github.com>
2020-11-03 19:52:55 +1100

    SimC-Snackbar-+-Dialog-Changes

--- src/Modules/SetupAndMenus/SimCraftDialog.js
+++ src/Modules/SetupAndMenus/SimCraftDialog.js
@@ -5,7 +5,7 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import {runSimC} from "../Engine/SimCImport/SimCImportEngine";
+import { runSimC } from "../Engine/SimCImport/SimCImportEngine";
 import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
 
 // const useStyles = makeStyles((theme) => ({
@@ -15,8 +15,8 @@ import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
 export default function SimCraftInput(props) {
   // const classes = useStyles;
   const [open, setOpen] = React.useState(false);
-  const [simC, setSimC] = React.useState('');
-  const [errorMessage, setErrorMessage] = React.useState('');
+  const [simC, setSimC] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -27,10 +27,17 @@ export default function SimCraftInput(props) {
   };
 
   const handleSubmit = () => {
-    runSimC(simC, props.pl, props.contentType, setErrorMessage);
+    runSimC(
+      simC,
+      props.pl,
+      props.contentType,
+      setErrorMessage,
+      props.simcSnack,
+      handleClose,
+      setSimC
+    );
     //setOpen(false);
   };
-
 
   return (
     <div>
@@ -46,7 +53,6 @@ export default function SimCraftInput(props) {
       >
         <DialogTitle id="form-dialog-title">Paste Your SimC String</DialogTitle>
         <DialogContent style={{ height: 400 }}>
-          
           <TextField
             // className={classes.root}
             autoFocus
@@ -57,12 +63,11 @@ export default function SimCraftInput(props) {
             fullWidth
             style={{ height: "100%" }}
             variant="outlined"
-            onChange={evt => setSimC(evt.target.value)}
+            onChange={(evt) => setSimC(evt.target.value)}
           />
-          
         </DialogContent>
         <DialogActions>
-        <p id="SimCError">{errorMessage}</p>
+          <p id="SimCError">{errorMessage}</p>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
