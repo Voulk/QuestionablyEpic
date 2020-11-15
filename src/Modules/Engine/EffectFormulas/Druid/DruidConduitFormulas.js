4b755632bc22a328e4ee47e6403a50fa59a8b870
VoulkInf3 <itsvoulk@gmail.com>
2020-11-14 21:43:26 -0600

    Minor

--- src/Modules/Engine/EffectFormulas/Druid/DruidConduitFormulas.js
+++ src/Modules/Engine/EffectFormulas/Druid/DruidConduitFormulas.js
@@ -1,18 +1,26 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
+
+
 
 export const getDruidConduit = (conduitID, pl, contentType, itemLevel = 156) => {
     let bonus_stats = {};
+    let conduitLevel = 1; // Convert the conduit item level to a rank. 
+    let expectedOverhealing = 0;
 
     // === Potency Conduits ===
     // Flash of Clarity
     if (conduitID === 340616) {
         bonus_stats.HPS = 999; // Placeholder tester. 
-        console.log("Sending back bonus stats");
 
     }
 
     // Floral Recycling
     else if (conduitID === 340621) {
-
+        // Assumptions
+        // - 
+        let trait_bonus = 0.36 + (conduitLevel * 0.04)
+        let hotConsumedAvgSP = 0.3 * 0.75; // Equal to 75% of a regrowth HoT. 
+        bonus_stats.HPS = hotConsumedAvgSP * trait_bonus * (1 - expectedOverhealing) * pl.getStatMultiplier(['haste', 'crit', 'mastery', 'versatility', 'intellect']);
     }
 
     // Ready for Anything
