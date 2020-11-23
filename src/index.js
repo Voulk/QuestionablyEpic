34e1ee8e963fc750d9ed61fc0d59f435037b1f8d
VoulkInf3 <itsvoulk@gmail.com>
2020-11-23 11:35:34 -0600

    Unregistered service worker.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker, {unregister} from './registerServiceWorker';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n'
 
