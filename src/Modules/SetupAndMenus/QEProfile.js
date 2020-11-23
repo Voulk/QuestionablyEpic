5dc6cd778478abeac3e9e008d2cd1fd98ecf6d56
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2020-11-23 12:13:14 +1100

    Email Field

--- src/Modules/SetupAndMenus/QEProfile.js
+++ src/Modules/SetupAndMenus/QEProfile.js
@@ -1,16 +1,67 @@
-import React, { Component, useEffect } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Button, Typography, TextField, Paper } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down("sm")]: {
+      margin: "auto",
+      width: "80%",
+      justifyContent: "center",
+    },
+    [theme.breakpoints.up("md")]: {
+      margin: "auto",
+      width: "65%",
+      justifyContent: "center",
+    },
+    [theme.breakpoints.up("lg")]: {
+      margin: "auto",
+      width: "45%",
+      justifyContent: "center",
+    },
+  },
+}));
 
-// Component Stub
-/*
-Consider swapping to a subscription page / button if there isn't a strong use of a profile in the first place. 
-*/
-export function QEProfile(props) {
+export default function QEProfile(props) {
+  const classes = useStyles();
+  const [email, setEmail] = React.useState("");
+  const emailHandler = (event) => {
+    setEmail(event);
+  };
 
-    return(
-        <div>
-
-        </div>
-    );
-
+  return (
+    <div className={classes.root}>
+      <Paper elevation={0} style={{ padding: 10 }}>
+        <Typography variant="h6" gutterBottom color="primary">
+          Enter your Email to link to Patreon
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          spacing={1}
+          wrap="nowrap"
+        >
+          <Grid item xs={11}>
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              value={email}
+              type="email"
+              fullWidth
+              size="small"
+              onChange={(e) => emailHandler(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs="auto">
+            <Button variant="contained" color="primary">
+              Save
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
 }
