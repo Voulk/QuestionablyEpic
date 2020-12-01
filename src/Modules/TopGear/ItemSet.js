483e97ef38f8dc8332658bbfb4bda171bf112c80
VoulkInf3 <itsvoulk@gmail.com>
2020-12-01 17:58:01 -0600

    Created an ItemSet class that represents a full collection of items such that you have one item in each slot.

---
+++ src/Modules/TopGear/ItemSet.js
@@ -0,0 +1,19 @@
+
+// Represents a full set of items. 
+
+class ItemSet {
+    constructor() {
+
+    }
+
+    id = 0;
+    sumSoftScore = 0;
+    itemList = [];
+
+    bonus_stats = {
+
+    }
+
+    
+
+}
--- src/Modules/TopGear/TopGear.js
+++ src/Modules/TopGear/TopGear.js
@@ -146,9 +146,7 @@ export default function TopGear(props) {
   // Popover Props
   const [anchorEl, setAnchorEl] = useState(null);
   // Define State.
-  const [itemLevel, setItemLevel] = useState("");
-  const [itemID, setItemID] = useState("");
-  const [itemName, setItemName] = useState("");
+
   const [activeSlot, setSlot] = useState("");
   const [itemSocket, setItemSocket] = useState("");
   const [itemTertiary, setItemTertiary] = useState("");
@@ -166,139 +164,9 @@ export default function TopGear(props) {
   // This is left as a TODO until key functionality is completed but is a moderate priority.
   // const itemTertiaries = [{"Avoidance", "Avoidance"},{"Leech", "Leech"}, {"None", ""}];
 
-  const handleClick = () => {
-    setOpen(true);
-  };
 
-  const handleClickDelete = () => {
-    setOpenDelete(true);
-  };
+  // Fill Items fills the ItemNames box with items appropriate to the given slot and spec.  };
 
-  const handleClosePop = () => {
-    setAnchorEl(null);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpen(false);
-  };
-
-  const handleCloseDelete = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpenDelete(false);
-  };
-
-  // Fill Items fills the ItemNames box with items appropriate to the given slot and spec.
-  const fillItems = (slotName, spec) => {
-    const acceptableArmorTypes = getValidArmorTypes(spec);
-    const acceptableWeaponTypes = getValidWeaponTypes(spec, slotName);
-    let newItemList = [];
-
-    itemDB
-      .filter(
-        (key) =>
-          (slotName === key.slot && key.slot === "Back") ||
-          (slotName === key.slot &&
-            key.itemClass === 4 &&
-            acceptableArmorTypes.includes(key.itemSubClass)) ||
-          (slotName === "Offhands" &&
-            (key.slot === "Holdable" ||
-              key.slot === "Offhand" ||
-              key.slot === "Shield")) ||
-          (slotName === "Weapons" &&
-            key.itemClass === 2 &&
-            acceptableWeaponTypes.includes(key.itemSubClass))
-      )
-      .map((key) =>
-        newItemList.push({ value: key.id, label: key.names[currentLanguage] })
-      );
-
-    newItemList.sort((a, b) => (a.label > b.label ? 1 : -1));
-    setItemDropdown(newItemList);
-  };
-
-  // Add an item to our "Active Items" array.
-  const addItem = (event) => {
-    if (itemLevel > 300) {
-      setAnchorEl(anchorEl ? null : event.currentTarget);
-      return null;
-    }
-    let player = props.pl;
-    let item = new Item(
-      itemID,
-      itemName,
-      getItemSlot(itemID),
-      itemSocket,
-      itemTertiary,
-      0,
-      itemLevel
-    );
-
-    item.level = itemLevel;
-    item.stats = calcStatsAtLevel(
-      itemLevel,
-      getItemSlot(itemID),
-      getItemAllocations(itemID),
-      itemTertiary
-    );
-
-    item.effect = getItemEffect(itemID);
-    item.softScore = scoreItem(item, player, props.contentType);
-    player.addActiveItem(item);
-    setItemList([...player.getActiveItems(activeSlot)]);
-    setOpen(true);
-  };
-
-  const deleteItem = (unique) => {
-    let player = props.pl;
-    player.deleteActiveItem(unique);
-    setItemList([...player.getActiveItems(activeSlot)]);
-    handleClickDelete();
-  };
-
-  const itemNameChanged = (event, val) => {
-    if (val === null) {
-      setItemID("");
-      setItemName("");
-    } else {
-      setItemID(val.value);
-      setItemName(val.name);
-    }
-  };
-
-  const itemLevelChanged = (val) => {
-    setItemLevel(val);
-    setItemSocket("");
-    setItemTertiary("");
-  };
-
-  const itemSocketChanged = (event) => {
-    setItemSocket(event.target.value);
-  };
-
-  const itemTertiaryChanged = (event) => {
-    setItemTertiary(event.target.value);
-  };
-
-  const changeSlot = (e, v) => {
-    if (v === null) {
-    } else {
-      setItemDropdown([]);
-      setAutoValue(v.value);
-      setInputValue("");
-      setItemLevel("");
-      setItemSocket("");
-      setItemTertiary("");
-    }
-
-    setSlot(e.target.value);
-    setItemList([...props.pl.getActiveItems(e.target.value)]);
-    fillItems(e.target.value, props.pl.spec);
-  };
 
   const activateItem = (unique) => {
     let player = props.pl;
