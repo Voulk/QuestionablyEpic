5fb69f86da84ac4cc83edab2fdd40b7dfb96df9f
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2020-12-17 00:58:33 +1100

    Small Updates

--- src/Modules/HallOfFame/HallOfFame.js
+++ src/Modules/HallOfFame/HallOfFame.js
@@ -8,6 +8,7 @@ import {
   DialogTitle,
   Typography,
   Grid,
+  Divider,
 } from "@material-ui/core";
 import { runSimC } from "../Engine/SimCImport/SimCImportEngine";
 import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
@@ -22,6 +23,16 @@ export default function HallOfFame(props) {
   const handleClose = () => {
     setOpen(false);
   };
+
+  let color = {
+    "Rolls Royce Edition": "#04E07C",
+    "Diamond Edition": "#FFB6C1",
+    "Gold Edition": "#DAA520",
+    "Standard Edition": "#FFFFFF",
+    "Basic Edition": "#FFFFFF",
+  };
+
+  let patronStatus = "Rolls Royce Edition";
 
   return (
     <div>
@@ -40,13 +51,15 @@ export default function HallOfFame(props) {
         fullWidth={true}
       >
         <DialogTitle
+          disableTypography
           color="primary"
           variant="h6"
           align="center"
           id="form-dialog-title"
           style={{ color: "#F2BF59" }}
         >
-          {t("HallOfFame.title")}
+          <Typography variant="h3">{t("HallOfFame.title")}</Typography>
+          <Divider />
         </DialogTitle>
 
         <DialogContent style={{ height: 400 }}>
@@ -57,10 +70,75 @@ export default function HallOfFame(props) {
             justify="center"
             alignItems="center"
           >
+            <Grid item xs={12}>
+              <Typography
+                variant="h6"
+                style={{
+                  color: "#04E07C",
+                }}
+                align="center"
+              >
+                Rolls Royce Patrons
+              </Typography>
+            </Grid>
             {names.map((key) => (
               <Grid item xs={2}>
                 <Typography
-                  style={{ border: "1px solid #F2BF59", borderRadius: 4 }}
+                  style={{
+                    border: "1px solid #04E07C",
+                    borderRadius: 4,
+                    color: color[patronStatus],
+                  }}
+                  align="center"
+                >
+                  {key}
+                </Typography>
+              </Grid>
+            ))}
+            <Grid item xs={12}>
+              <Typography
+                variant="h6"
+                style={{
+                  color: "#FFB6C1",
+                }}
+                align="center"
+              >
+                Diamond Patrons
+              </Typography>
+            </Grid>
+            {names.map((key) => (
+              <Grid item xs={2}>
+                <Typography
+                  style={{
+                    border: "1px solid #FFB6C1",
+                    borderRadius: 4,
+                    color: "#FFB6C1",
+                  }}
+                  align="center"
+                >
+                  {key}
+                </Typography>
+              </Grid>
+            ))}
+            <Grid item xs={12}>
+              <Typography
+                variant="h6"
+                style={{
+                  color: "#DAA520",
+                }}
+                align="center"
+              >
+                Gold Patrons
+              </Typography>
+            </Grid>
+            {names.map((key) => (
+              <Grid item xs={2}>
+                <Typography
+                  style={{
+                    border: "1px solid #DAA520",
+                    borderRadius: 4,
+                    color: "#DAA520",
+                  }}
                   align="center"
                 >
                   {key}
