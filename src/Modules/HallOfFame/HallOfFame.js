76830240b84ee2f6d77449bbc974306edc84532c
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2020-12-19 02:13:43 +1100

    Potential Changes

--- src/Modules/HallOfFame/HallOfFame.js
+++ src/Modules/HallOfFame/HallOfFame.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import {
+  AppBar,
   Button,
   Dialog,
   DialogActions,
@@ -9,6 +10,7 @@ import {
   Typography,
   Grid,
   Divider,
+  Paper,
 } from "@material-ui/core";
 import { runSimC } from "../Engine/SimCImport/SimCImportEngine";
 import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
@@ -58,23 +60,24 @@ export default function HallOfFame(props) {
         fullWidth={true}
         scroll="body"
       >
-        <DialogTitle
+        <AppBar position="static" elevation={1}>
+          {/* <DialogTitle
           disableTypography
           color="primary"
           variant="h6"
           align="center"
           id="form-dialog-title"
-        >
-          <Typography color="primary" variant="h3">
+        > */}
+          <Typography align="center" color="secondary" variant="h4">
             {t("HallOfFame.title")}
           </Typography>
-          <Divider variant="middle" />
-          <Typography style={{ color: "limegreen" }} variant="h6">
-            Below are the mighty patrons of Questionably Epic, without their
-            contributions this site could not continue. As thanks we name them
-            for their contributions
-          </Typography>
-        </DialogTitle>
+        </AppBar>
+        <Typography align="center" style={{ color: "limegreen" }} variant="h6">
+          Below are the mighty patrons of Questionably Epic, without their
+          contributions this site could not continue. As thanks we name them for
+          their contributions
+        </Typography>
+        {/* </DialogTitle> */}
 
         <DialogContent style={{ height: 400 }}>
           <Grid
@@ -85,89 +88,161 @@ export default function HallOfFame(props) {
             alignItems="center"
           >
             <Grid item xs={12}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "#04E07C",
-                  border: "1px solid #04E07C",
-                  borderRadius: 4,
-                }}
-                align="center"
+              <Grid
+                container
+                spacing={1}
+                direction="row"
+                justify="flex-start"
+                alignItems="center"
               >
-                Rolls Royce Patrons
-              </Typography>
-            </Grid>
-            {names
-              .filter((key) => key.tier === "Rolls Royce")
-              .map((key) => (
-                <Grid item xs={4}>
-                  <Typography
-                    style={{
-                      borderRadius: 4,
-                      color: "#04E07C",
-                    }}
-                    align="center"
-                  >
-                    {key.name}
-                  </Typography>
+                <Grid item xs={12}>
+                  <Paper elevation={1} style={{ backgroundColor: "#525252" }}>
+                    <AppBar
+                      elevation={0}
+                      position="static"
+                      style={{
+                        backgroundColor: "#04E07C",
+                        borderRadius: "4px 4px 0px 0px",
+                      }}
+                    >
+                      <Typography variant="h6" align="center">
+                        Rolls Royce Patrons
+                      </Typography>
+                    </AppBar>
+
+                    <Grid item xs={12}>
+                      <Grid
+                        container
+                        spacing={1}
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="center"
+                      >
+                        {names
+                          .filter((key) => key.tier === "Rolls Royce")
+                          .map((key) => (
+                            <Grid item xs={4}>
+                              <Typography
+                                style={{
+                                  borderRadius: 4,
+                                  color: "#04E07C",
+                                }}
+                                align="center"
+                              >
+                                {key.name}
+                              </Typography>
+                            </Grid>
+                          ))}
+                      </Grid>
+                    </Grid>
+                  </Paper>
                 </Grid>
-              ))}
+              </Grid>
+            </Grid>
             <Grid item xs={12}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "#FFB6C1",
-                  border: "1px solid #FFB6C1",
-                  borderRadius: 4,
-                }}
-                align="center"
+              <Grid
+                container
+                spacing={1}
+                direction="row"
+                justify="flex-start"
+                alignItems="center"
               >
-                Diamond Patrons
-              </Typography>
-            </Grid>
-            {names
-              .filter((key) => key.tier === "Diamond")
-              .map((key) => (
-                <Grid item xs={4}>
-                  <Typography
-                    style={{
-                      borderRadius: 4,
-                      color: "#FFB6C1",
-                    }}
-                    align="center"
-                  >
-                    {key.name}
-                  </Typography>
+                <Grid item xs={12}>
+                  <Paper elevation={1} style={{ backgroundColor: "#525252" }}>
+                    <AppBar
+                      elevation={0}
+                      position="static"
+                      style={{
+                        backgroundColor: "#FFB6C1",
+                        borderRadius: "4px 4px 0px 0px",
+                      }}
+                    >
+                      <Typography variant="h6" align="center">
+                        Diamond Patrons
+                      </Typography>
+                    </AppBar>
+
+                    <Grid item xs={12}>
+                      <Grid
+                        container
+                        spacing={1}
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="center"
+                      >
+                        {names
+                          .filter((key) => key.tier === "Diamond")
+                          .map((key) => (
+                            <Grid item xs={4}>
+                              <Typography
+                                style={{
+                                  borderRadius: 4,
+                                  color: "#FFB6C1",
+                                }}
+                                align="center"
+                              >
+                                {key.name}
+                              </Typography>
+                            </Grid>
+                          ))}
+                      </Grid>
+                    </Grid>
+                  </Paper>
                 </Grid>
-              ))}
+              </Grid>
+            </Grid>
             <Grid item xs={12}>
-              <Typography
-                variant="h6"
-                style={{
-                  color: "#DAA520",
-                  border: "1px solid #DAA520",
-                  borderRadius: 4,
-                }}
-                align="center"
+              <Grid
+                container
+                spacing={1}
+                direction="row"
+                justify="flex-start"
+                alignItems="center"
               >
-                Gold Patrons
-              </Typography>
-            </Grid>
-            {names
-              .filter((key) => key.tier === "Gold")
-              .map((key) => (
-                <Grid item xs={4}>
-                  <Typography
-                    style={{
-                      borderRadius: 4,
-                      color: "#DAA520",
-                    }}
-                    align="center"
-                  >
-                    {key.name}
-                  </Typography>
+                <Grid item xs={12}>
+                  <Paper elevation={1} style={{ backgroundColor: "#525252" }}>
+                    <AppBar
+                      elevation={0}
+                      position="static"
+                      style={{
+                        backgroundColor: "#DAA520",
+                        borderRadius: "4px 4px 0px 0px",
+                      }}
+                    >
+                      <Typography variant="h6" align="center">
+                        Gold
+                      </Typography>
+                    </AppBar>
+
+                    <Grid item xs={12}>
+                      <Grid
+                        container
+                        spacing={1}
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="center"
+                      >
+                        {names
+                          .filter((key) => key.tier === "Gold")
+                          .map((key) => (
+                            <Grid item xs={4}>
+                              <Typography
+                                style={{
+                                  borderRadius: 4,
+                                  color: "#DAA520",
+                                }}
+                                align="center"
+                              >
+                                {key.name}
+                              </Typography>
+                            </Grid>
+                          ))}
+                      </Grid>
+                    </Grid>
+                  </Paper>
                 </Grid>
-              ))}
+              </Grid>
+            </Grid>
           </Grid>
         </DialogContent>
 
