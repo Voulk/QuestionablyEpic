39d0a7a6c2b06f5843f374d3ac0a50bcbab10cd0
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2020-12-22 20:16:52 +1100

    Update

--- src/Modules/Charts/VerticalChart.js
+++ src/Modules/Charts/VerticalChart.js
@@ -20,35 +20,43 @@ export default class VerticalChart extends PureComponent {
     super();
   }
 
-  drawAreas() {
-    const Ilvls = ["i226", "i213", "i200", "i187", "i174", "i161"];
-    let barArr = [];
-    let len = Ilvls.length;
-    let colorCodes = chroma.scale("Paired").colors(len);
+  // drawAreas() {
+  //   const Ilvls = ["i226", "i213", "i200", "i187", "i174", "i161"];
+  //   let barArr = [];
+  //   let len = Ilvls.length;
+  //   let colorCodes = chroma.scale("Accent").colors(len);
 
-    // Pushes Each datakey for ability with a colour from the generated scale to an array for the chart.
-    for (let i in Ilvls) {
-      if (Ilvls.hasOwnProperty(i)) {
-        barArr.push(
-          <Bar
-            dataKey={Ilvls[i]}
-            fill={colorCodes[i]}
-            barSize={10}
-            opacity={1}
-          />
-        );
-      }
-    }
-    return barArr;
-  }
+  //   // Pushes Each datakey for ability with a colour from the generated scale to an array for the chart.
+  //   for (let i in Ilvls) {
+  //     if (Ilvls.hasOwnProperty(i)) {
+  //       barArr.push(
+  //         <Bar
+  //           dataKey={Ilvls[i]}
+  //           fill={colorCodes[i]}
+  //           barSize={10}
+  //           opacity={1}
+  //         />
+  //       );
+  //     }
+  //   }
+  //   return barArr;
+  // }
 
   render(props) {
+    const Ilvls = ["i226", "i213", "i200", "i187", "i174", "i161"];
+    let len = Ilvls.length;
+    let colorCodes = chroma.scale("Accent").colors(len);
     return (
-      <ResponsiveContainer className="ResponsiveContainer" aspect={1 / 1}>
+      <ResponsiveContainer
+        className="ResponsiveContainer"
+        width="100%"
+        height={2000}
+      >
         <BarChart
           data={this.props.data}
-          barGap={-10}
-          barCategoryGap={10}
+          barSize={15}
+          barGap={-2}
+          // barCategoryGap={10}
           layout="vertical"
           margin={{
             top: 20,
@@ -56,6 +64,7 @@ export default class VerticalChart extends PureComponent {
             bottom: 20,
             left: 200,
           }}
+          reverseStackOrder={true}
         >
           <Tooltip
             labelStyle={{ color: "#ffffff" }}
@@ -67,8 +76,6 @@ export default class VerticalChart extends PureComponent {
             wrapperStyle={{ color: "#1b1b1b" }}
           />
           <XAxis type="number" stroke="#f5f5f5" />
-          <XAxis type="number" stroke="#f5f5f5" orientation="top" />
-
           <Legend verticalAlign="top" />
           <CartesianGrid vertical={true} horizontal={false} />
           <YAxis
@@ -77,9 +84,20 @@ export default class VerticalChart extends PureComponent {
             stroke="#f5f5f5"
             interval={0}
             tick={{ width: 300 }}
-            maxTickGap={10}
+            yAxisId={0}
           />
-          {this.drawAreas()}
+          <YAxis type="category" dataKey="name" yAxisId={1} hide />
+          <YAxis type="category" dataKey="name" yAxisId={2} hide />
+          <YAxis type="category" dataKey="name" yAxisId={3} hide />
+          <YAxis type="category" dataKey="name" yAxisId={4} hide />
+          <YAxis type="category" dataKey="name" yAxisId={5} hide />
+
+          <Bar dataKey={"i226"} fill={colorCodes[0]} yAxisId={0} />
+          <Bar dataKey={"i213"} fill={colorCodes[1]} yAxisId={1} />
+          <Bar dataKey={"i200"} fill={colorCodes[2]} yAxisId={2} />
+          <Bar dataKey={"i187"} fill={colorCodes[3]} yAxisId={3} />
+          <Bar dataKey={"i174"} fill={colorCodes[4]} yAxisId={4} />
+          <Bar dataKey={"i161"} fill={colorCodes[5]} yAxisId={5} />
         </BarChart>
       </ResponsiveContainer>
     );
