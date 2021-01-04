aaa2eabe1cced5aa5594f7ac7116287558ad405e
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-01-05 00:33:11 +1100

    Upgrade Finder Front

--- src/Modules/UpgradeFinder/UpgradeFinderFront.js
+++ src/Modules/UpgradeFinder/UpgradeFinderFront.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Grid, Typography } from "@material-ui/core";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { Paper, Grid, Typography, Slider, Divider } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Button from "@material-ui/core/Button";
+import HelpText from "../SetupAndMenus/HelpText";
 
 import { withStyles } from "@material-ui/core/styles";
 import FormGroup from "@material-ui/core/FormGroup";
@@ -18,15 +25,89 @@ const useStyles = makeStyles((theme) => ({
     display: "block",
     marginLeft: "auto",
     marginRight: "auto",
-    flexGrow: 1,
     maxWidth: "55%",
   },
+  slider: {
+    width: "90%",
+    margin: "0px 20px 50px 20px",
+    textAlign: "center",
+  },
+  button: {
+    width: 200,
+    height: 75,
+  },
 }));
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiSlider: {
+      markLabel: {
+        width: 50,
+        whiteSpace: "wrap",
+        textAlign: "center",
+        color: "#fff",
+      },
+      markLabelActive: {
+        color: "#F2BF59",
+      },
+    },
+  },
+});
+
+const raidDifficulty = ["Raid Finder", "Normal", "Heroic", "Mythic"];
+const pvpCurrency = ["Honor", "Conquest"];
+
+const marks = [
+  {
+    value: 0,
+    label: "M0 184ilvl",
+  },
+  {
+    value: 1,
+    label: "M2 187ilvl",
+  },
+  {
+    value: 2,
+    label: "M3 190ilvl",
+  },
+  {
+    value: 4,
+    label: "M4-5 193ilvl",
+  },
+  {
+    value: 5,
+    label: "M6 197ilvl",
+  },
+  {
+    value: 8,
+    label: "M7-9 200ilvl",
+  },
+  {
+    value: 10,
+    label: "M10-11 203ilvl",
+  },
+  {
+    value: 13,
+    label: "M12-14 207ilvl",
+  },
+  {
+    value: 14,
+    label: "M15 210ilvl",
+  },
+];
+
+function valuetext(value) {
+  return `${value}`;
+}
+
+function valueLabelFormat(value) {
+  return marks.findIndex((mark) => mark.value === value) + 1;
+}
 
 export default function UpgradeFinderFront(props) {
   const classes = useStyles();
   const { t, i18n } = useTranslation();
-
+  const helpText = t("UpgradeFinderFront.HelpText");
   return (
     <div className={classes.root}>
       <Typography
@@ -37,275 +118,90 @@ export default function UpgradeFinderFront(props) {
       >
         {t("UpgradeFinderFront.Header")}
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item container spacing={1}>
-          <Paper elevation={0}>
-            <Grid item xs={12}>
-              <Typography
-                color="primary"
-                align="center"
-                style={{ padding: "10px 10px 5px 10px" }}
-              >
-                {t("UpgradeFinderFront.RaidHeaderSettings")}
-              </Typography>
-            </Grid>
-            <Grid container item xs={12} spacing={1}>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Raid Finder"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Normal"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Heroic"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Mythic"
-                />
-              </Grid>
+
+      <Grid container spacing={2} style={{ marginTop: 20 }}>
+        <Grid item xs={12}>
+          <HelpText text={helpText} />
+        </Grid>
+        <Grid item xs={12} spacing={1}>
+          <Paper elevation={0} style={{ padding: 10 }}>
+            <Typography
+              color="primary"
+              align="center"
+              variant="h4"
+              style={{ padding: "10px 10px 5px 10px" }}
+            >
+              {t("UpgradeFinderFront.RaidDifficultyHeader")}
+            </Typography>
+
+            <Grid container justify="center" spacing={1}>
+              {raidDifficulty.map((key) => (
+                <Grid item justify="center" xs="auto">
+                  <Button
+                    className={classes.button}
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                  >
+                    {key}
+                  </Button>
+                </Grid>
+              ))}
             </Grid>
           </Paper>
         </Grid>
 
-        <Grid item container spacing={1}>
-          <Paper elevation={0}>
-            <Grid item xs={12}>
-              <Typography
-                color="primary"
-                align="center"
-                style={{ padding: "10px 10px 5px 10px" }}
-              >
-                {t("UpgradeFinderFront.RaidHeaderSettings")}
-              </Typography>
-            </Grid>
-            <Grid container item xs={12} spacing={1}>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Raid Finder"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Normal"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Heroic"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Mythic"
-                />
-              </Grid>
-            </Grid>
+        <Grid item xs={12}>
+          <Paper elevation={0} style={{ textAlign: "center" }}>
+            <Typography
+              color="primary"
+              align="center"
+              variant="h4"
+              style={{ padding: "10px 10px 5px 10px" }}
+            >
+              {t("UpgradeFinderFront.MythicPlusHeader")}
+            </Typography>
+
+            <ThemeProvider theme={theme}>
+              <Slider
+                className={classes.slider}
+                style={{ color: "#52af77" }}
+                defaultValue={0}
+                valueLabelFormat={valueLabelFormat}
+                getAriaValueText={valuetext}
+                aria-labelledby="discrete-slider-restrict"
+                step={null}
+                valueLabelDisplay="off"
+                marks={marks}
+                max={14}
+              />
+            </ThemeProvider>
           </Paper>
         </Grid>
+        <Grid item xs={12} spacing={1}>
+          <Paper elevation={0} style={{ padding: 10 }}>
+            <Typography
+              color="primary"
+              align="center"
+              variant="h4"
+              style={{ padding: "10px 10px 5px 10px" }}
+            >
+              {t("UpgradeFinderFront.PvPHeader")}
+            </Typography>
 
-        <Grid item container spacing={1}>
-          <Paper elevation={0}>
-            <Grid item xs={12}>
-              <Typography
-                color="primary"
-                align="center"
-                style={{ padding: "10px 10px 5px 10px" }}
-              >
-                {t("UpgradeFinderFront.RaidHeaderSettings")}
-              </Typography>
-            </Grid>
-            <Grid container item xs={12} spacing={1}>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Raid Finder"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Normal"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Heroic"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Mythic"
-                />
-              </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
-
-        <Grid item container spacing={1}>
-          <Paper elevation={0}>
-            <Grid item xs={12}>
-              <Typography
-                color="primary"
-                align="center"
-                style={{ padding: "10px 10px 5px 10px" }}
-              >
-                {t("UpgradeFinderFront.RaidHeaderSettings")}
-              </Typography>
-            </Grid>
-            <Grid container item xs={12} spacing={1}>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Raid Finder"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Normal"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Heroic"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      //   checked={state.checkedB}
-                      //   onChange={handleChange}
-                      name="checkedB"
-                      color="primary"
-                    />
-                  }
-                  label="Mythic"
-                />
-              </Grid>
+            <Grid container justify="center" spacing={1}>
+              {pvpCurrency.map((key) => (
+                <Grid item justify="center" xs="auto">
+                  <Button
+                    className={classes.button}
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                  >
+                    {key}
+                  </Button>
+                </Grid>
+              ))}
             </Grid>
           </Paper>
         </Grid>
