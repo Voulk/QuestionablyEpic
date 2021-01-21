182f6afefbd62fa10ae420a185f252d49ac34e2a
VoulkInf3 <itsvoulk@gmail.com>
2021-01-21 11:49:00 -0600

    Bugfix: Changing weights then swapping content types would display the wrong weights when you went to edit.

--- src/Modules/SetupAndMenus/CharComponentGen.js
+++ src/Modules/SetupAndMenus/CharComponentGen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "@material-ui/core/Card";
 import {
   Paper,
@@ -149,6 +149,7 @@ export default function CharCards(props) {
 
   const { t } = useTranslation();
   const classes = useStyles();
+
   const [value, setValue] = React.useState(0);
   const [region, setRegion] = React.useState(player.region);
   const [open, setOpen] = React.useState(false);
@@ -193,6 +194,12 @@ export default function CharCards(props) {
   };
   const handleClickOpen = (e) => {
     e.preventDefault();
+    
+    setCritical(player.getStatWeight(props.contentType, STAT.CRITICAL_STRIKE));
+    setHaste(player.getStatWeight(props.contentType, STAT.HASTE));
+    setMastery(player.getStatWeight(props.contentType, STAT.MASTERY));
+    setVersatility(player.getStatWeight(props.contentType, STAT.VERSATILITY));
+    setLeech(player.getStatWeight(props.contentType, STAT.LEECH));
     setOpen(true);
   };
   const handleClose = () => {
