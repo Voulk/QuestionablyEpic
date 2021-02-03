83a8baa2d89643921d325b2950782a73c1ab3fb2
VoulkInf3 <itsvoulk@gmail.com>
2021-02-03 13:08:50 -0600

    Minor.

--- src/Modules/Engine/EffectFormulas/Generic/TrinketEffectFormulas.js
+++ src/Modules/Engine/EffectFormulas/Generic/TrinketEffectFormulas.js
@@ -158,7 +158,7 @@ export function getTrinketEffect(effectName, player, contentType, itemLevel) {
     let effect = activeTrinket.effects[0];
     let playerBestSecondary = player.getHighestStatWeight(contentType, ["versatility"]); // Exclude Vers since there isn't a Vers version.
 
-    bonus_stats[playerBestSecondary] = ((getProcessedValue(effect.coefficient, effect.table, itemLevel) * effect.duration) / effect.cooldown) * 0.95;
+    bonus_stats[playerBestSecondary] = ((getProcessedValue(effect.coefficient, effect.table, itemLevel) * effect.duration) / effect.cooldown) * 0.9;
     // power reduced by 5% because of the chance something interferes. This needs to be much much better and I'll fix it up this week.
   
   } else if (effectName === "Flame of Battle") {
