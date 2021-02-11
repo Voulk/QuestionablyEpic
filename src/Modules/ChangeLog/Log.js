572523021c1615951187557a81c6197ccb0a1c1a
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-02-12 07:35:19 +1100

    update Log.js

--- src/Modules/ChangeLog/Log.js
+++ src/Modules/ChangeLog/Log.js
@@ -14,9 +14,10 @@ export const changeLog = [
   date: "11-02-2021",
   changes: [
   "Implemented Changelog for better transparency to Users",
-  "Fixed Venthyr Paladin Covenant Ability score that was showing 'NAN' instead of HPS",
-  "Fixed Necrolord Monk 'Bone Marrow Hops' conduit to correctly use the 50% hit chance of 'Bonedust Brew'",
-  "Updated Summary Panel on the Covenant Exploration page to be clearer on what it represents"
+  "Updated Summary Panel on the Covenant Exploration page to be clearer on what it represents",
+  "Venthyr Paladin Covenant Ability score was showing 'NAN' now correctly shows HPS",
+  "Necrolord Monk 'Bone Marrow Hops' conduit now correctly uses the 50% hit chance of 'Bonedust Brew'",
+  "General Draven's 'Hold Your Ground' has been updated to better reflect real world uuptimes (Paladin: X, Druid: X, Shaman: X, Monk: X, Holy Priest: X, Discipline Priest: X)"
   ]
   },
 ];
