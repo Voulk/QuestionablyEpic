4cbc1dae32481f56f4544d22f89dc908ece089fc
VoulkInf3 <itsvoulk@gmail.com>
2021-02-26 23:05:08 -0600

    Minor.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker, { unregister } from "./registerServiceWorker";
 import { I18nextProvider } from "react-i18next";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import i18n from "./i18n";
 
+const initialState = {
+  gameType: "Retail",
+};
+
+function reducer(state = initialState, action) {
+  console.log('reducer', state, action);
+  return state;
+}
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>,
+  <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </Provider>,
   document.getElementById("root"),
 );
 unregister();
