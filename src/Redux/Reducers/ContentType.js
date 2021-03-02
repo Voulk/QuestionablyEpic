162662961040ff18af1067e74fc9ababa07a5aa8
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-03-03 00:57:48 +1100

    Code Rework

--- src/Redux/Reducers/ContentType.js
+++ src/Redux/Reducers/ContentType.js
@@ -1,17 +1,12 @@
 import ls from "local-storage";
 
-export default function (state = initialState, action) {
-  switch (action.type) {
-    case TOGGLE_CONTENT: {
-      const { content } = action.payload;
-      const toggleType = content === "Raid" ? "Dungeon" : "Raid";
-      ls.set("contentType", toggleType);
-      return {
-        ...state,
-        contentType: toggleType,
-      };
-    }
-    default:
-      return state;
-  }
+// Action to toggle content type in the Redux store.
+export function contentType(state, action) {
+  const { content } = action.payload;
+  const toggleType = content === "Raid" ? "Dungeon" : "Raid";
+  ls.set("contentType", toggleType);
+  return {
+    ...state,
+    contentType: toggleType,
+  };
 }
--- src/Redux/Reducers/RootReducer.js
+++ src/Redux/Reducers/RootReducer.js
@@ -1,25 +1,23 @@
-import { TOGGLE_CONTENT } from "../ActionTypes"
-import ls from "local-storage";
+// Action Types
+import { TOGGLE_CONTENT } from "../ActionTypes";
 
+// Reducers
+import { contentType } from "./ContentType";
+
+// Initial Store/State settings
 const initialState = {
-    gameType: "Retail",
-    contentType: "Raid",
-  };
-  
- export default function rootReducer(state = initialState, action) {
-    console.log('reducer', state, action);
-  
-    switch (action.type) {
-      case TOGGLE_CONTENT: {
-        const { content } = action.payload;
-        const toggleType = content === "Raid" ? "Dungeon" : "Raid";
-        ls.set("contentType", toggleType);
-        return {
-          ...state,
-          contentType: toggleType,
-        };
-      }
-      default:
-    return state;
+  gameType: "Retail",
+  contentType: "Raid",
+};
+
+export default function rootReducer(state = initialState, action) {
+  console.log("reducer", state, action);
+
+  switch (action.type) {
+    case TOGGLE_CONTENT:
+      return contentType(state, action);
+
+    default:
+      return state;
   }
-  }
+}
