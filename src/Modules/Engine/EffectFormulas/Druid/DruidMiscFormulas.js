1b9fa39d345420576bef0f4a759a00a592d3301b
VoulkInf3 <itsvoulk@gmail.com>
2021-03-22 15:47:43 -0500

    Updated the druid legendary log formulas for DTL & Memory of the Mother Tree.

---
+++ src/Modules/Engine/EffectFormulas/Druid/DruidMiscFormulas.js
@@ -0,0 +1,22 @@
+
+// Partially implemented. Complete if spells other than Rejuv are required.
+export function processDruidRawHealing(player, spellID) {
+
+    const isAwakening = true;
+
+    // Tree of Life
+    const treeUptime = 30 / 180;
+    const treeHealingIncreases = {774: 1.15 * 1.5, 48438: 8/6 * 1.15}
+    const treeHealingInc = (treeHealingIncreases[spellID] * treeUptime + (1 - treeUptime));
+
+    // Flourish
+    const flourishUptime = 8 / 90;
+    const flourishHealingIncreases = {774: 2};
+    const flourishHealingInc = flourishHealingIncreases[spellID] * flourishUptime + (1 - flourishUptime);
+
+
+  
+    console.log("Tree: " + treeHealingInc + ". Flourish: " + flourishHealingInc);
+    return (treeHealingInc * flourishHealingInc);
+  
+  }
