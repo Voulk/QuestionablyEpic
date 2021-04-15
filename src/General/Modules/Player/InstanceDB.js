eada53b3432eb41fe3019e238b8d41ec60f71f68
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-04-16 00:18:46 +1000

    Sanctum of Domination Raid ID's/Translations Added

--- src/General/Modules/Player/InstanceDB.js
+++ src/General/Modules/Player/InstanceDB.js
@@ -8,6 +8,7 @@ export const instanceDB = {
 };
 
 export const encounterDB = {
+  /* --------------------------------------- Castle Nathria --------------------------------------- */
   1190: [
     2393, // "Shriekwing",
     2429, // "Huntsman Altimor",
@@ -21,12 +22,31 @@ export const encounterDB = {
     2424, // "Sire Denathrius",
     999, // BoEs,
   ],
+
+  /* ---------------------------------------- World Bosses ---------------------------------------- */
+
   1192: [
     2430, // "Valinor, the Light of Eons",
     2431, //"Mortanis",
     2432, // "Oranomonos the Everbranching",
     2433, //"Nurgash Muckformed",
   ],
+
+  /* ------------------------------------ Sanctum of Domination ----------------------------------- */
+  1193: [
+    2435, // The Tarragrue
+    2442, // The Eye of the Jailer
+    2439, // The Nine
+    2444, // Remnant of Ner'zhul
+    2445, // Soulrender Dormazain
+    2443, // Painsmith Raznal
+    2446, // Guardian of the First Ones
+    2447, // Fatescribe Roh-Kalo
+    2440, // Kel'Thuzad
+    2441, // Sylvanas Windrunner
+  ],
+
+  /* ------------------------------------------ Dungeons ------------------------------------------ */
   1: [
     1182, //"Necrotic Wake",
     1183, //"Plaguefall",
@@ -38,5 +58,6 @@ export const encounterDB = {
     1189, //"Sanguine Depths",
   ],
 
+  /* --------------------------------------------- PVP -------------------------------------------- */
   2: [-17, -16],
 };
