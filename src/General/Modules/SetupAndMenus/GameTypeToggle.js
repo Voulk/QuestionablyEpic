c899fc6c3b1d993d8cb1925bdbbf48f94d68fdf0
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-05-06 23:31:15 +1000

    FInished Toggle Styling

--- src/General/Modules/SetupAndMenus/GameTypeToggle.js
+++ src/General/Modules/SetupAndMenus/GameTypeToggle.js
@@ -6,11 +6,32 @@ import { useDispatch } from "react-redux";
 import { toggleGameType } from "../../../Redux/Actions";
 import { useTranslation } from "react-i18next";
 import { Tooltip, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    /* ----------------------------------- Unselected button style ---------------------------------- */
+    minWidth: 300,
+    padding: 4,
+    opacity: 0.3,
+    /* ------------------------------------ Selected button style ----------------------------------- */
+    "&.MuiToggleButton-root.Mui-selected": {
+      minWidth: 300,
+      padding: 4,
+      opacity: 1,
+    },
+  },
+});
 
 export default function GameTypeSwitch() {
   const dispatch = useDispatch();
   const { t } = useTranslation();
+  const classes = useStyles();
+
+  /* ------------------------------- Current gameType in redux state ------------------------------ */
   const gameType = useSelector((state) => state.gameType);
+
+  /* ---------------------------------- Toggle gameType via redux --------------------------------- */
   const handleContent = (event, gameType) => {
     if (gameType === null) {
     } else {
@@ -20,24 +41,24 @@ export default function GameTypeSwitch() {
 
   return (
     <ToggleButtonGroup value={gameType} exclusive onChange={handleContent} aria-label="gameToggle" size="large">
-      <ToggleButton style={{ minWidth: 300, padding: 4 }} value="Classic" aria-label="classicLabel">
+      {/* ---------------------------------------------------------------------------------------------- */
+      /*                            Burning Crusade: Classic Game Type Toggle                           */
+      /* ---------------------------------------------------------------------------------------------- */}
+      <ToggleButton className={classes.root} value="Classic" aria-label="classicLabel">
         <Tooltip title={t("QeHeader.Tooltip.ChangeToDungeon")} arrow>
           <div style={{ display: "inline-flex" }}>
             <img src={require("../../../Images/Logos/Logo_BurningCrusade.png").default} alt={t("Burning Crusade")} />
-            {/* <Typography variant="button" noWrap>
-              {t("Burning Crusade")}
-            </Typography> */}
           </div>
         </Tooltip>
       </ToggleButton>
 
-      <ToggleButton style={{ minWidth: 300, padding: 4 }} value="Retail" aria-label="retailLabel">
+      {/* ---------------------------------------------------------------------------------------------- */
+      /*                                  Shadowlands Game Type Toggle                                  */
+      /* ----------------------------------------------------------------------------------------------  */}
+      <ToggleButton className={classes.root} value="Retail" aria-label="retailLabel">
         <Tooltip title={t("QeHeader.Tooltip.ChangeToRaid")} arrow>
           <div style={{ display: "inline-flex" }}>
             <img src={require("../../../Images/Logos/Logo_Shadowlands.png").default} alt={t("Shadowlands")} />
-            {/* <Typography variant="button" noWrap>
-              {t("Shadowlands")}
-            </Typography> */}
           </div>
         </Tooltip>
       </ToggleButton>
