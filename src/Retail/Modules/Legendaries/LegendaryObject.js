fc278afd19c6cef9b83827fd4a29a37f77c6526e
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-06-28 20:19:29 +1000

    MInor Change

--- src/Retail/Modules/Legendaries/LegendaryObject.js
+++ src/Retail/Modules/Legendaries/LegendaryObject.js
@@ -74,7 +74,7 @@ export default function LegendaryObject(props) {
               >
                 {legendaryDataObject.name}
                 {covenantSigils[legendaryDataObject.covenant] !== undefined ? (
-                  <img height={40} style={{ float: "right", position: "absolute", marginTop: -7, marginLeft: 10 }} src={covenantSigils[legendaryDataObject.covenant]} alt={t("Covenants.Kyrian")} />
+                  <img height={40} style={{ float: "right", position: "absolute", marginTop: -7, marginLeft: 10 }} src={covenantSigils[legendaryDataObject.covenant]} alt={t("Covenants." + legendaryDataObject.covenant)} />
                 ) : (
                   ""
                 )}
