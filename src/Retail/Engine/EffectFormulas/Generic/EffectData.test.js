69c9be2b30b69db7050789eda94fcafaf4e37980
Voulk <itsvoulk@gmail.com>
2021-07-04 13:30:27 -0500

    Brief start to Dom Sockets.

--- src/Retail/Engine/EffectFormulas/Generic/EffectData.test.js
+++ src/Retail/Engine/EffectFormulas/Generic/EffectData.test.js
@@ -20,3 +20,20 @@ describe("Passable Credentials Data Check", () => {
         expect(getProcessedValue(effect.coefficient, effect.table, level)).toBe(expectedResult);
     });
 });
+
+describe("Shard of Zed Data Check", () => {
+    // Raw trinket values are compared to our spell data. Efficiency excluded.
+    const activeEffect = effectData.find((effect) => effect.name === "Shard of Zed");
+    const effect = activeEffect.effects[0];
+
+    each`
+    rank   | expectedResult
+    ${4}  | ${25}
+    ${3}  | ${20}
+    ${0}  | ${17}
+    // add new test cases here
+    `.test("Shard of Zed Test - $rank - Expects: $expectedResult", ({ rank, expectedResult }) => {
+  
+        expect(getProcessedValue(effect.coefficient[rank], effect.table, 200)).toBe(expectedResult);
+    });
+});
