6a2f319cfe15f4467b8cba5d492cdf18d6dd6f5f
Voulk <itsvoulk@gmail.com>
2021-07-15 16:31:29 -0500

    Fixed a bug where sets wouldn't always register.

--- src/Retail/Engine/EffectFormulas/Generic/GenericEffectFormulas.js
+++ src/Retail/Engine/EffectFormulas/Generic/GenericEffectFormulas.js
@@ -59,6 +59,7 @@ export function getDominationGemEffect(effectName, player, contentType, rank) {
 
   else if (effectName === "Blood Link") {
     const effect = activeEffect.effects[0];
+    console.log("Rank: " + rank);
     const value = Math.round(getProcessedValue(effect.coefficient[rank], effect.table, 174, 1, false)) * player.getStatPerc("Vers") * player.getStatPerc("Crit")
     bonus_stats.hps = value * effect.ppm * (1 - effect.expectedOverhealing) * player.getStatPerc("Vers") / 60; // The healing effect basically scales with Vers twice.
     bonus_stats.dps = value * effect.ppm / 60;
