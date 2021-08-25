0362c837184f4e9ddb34ad30bfbd11565ed6af5c
Voulk <itsvoulk@gmail.com>
2021-08-25 12:34:10 -0500

    Clean up and log.

--- src/General/Modules/ChangeLog/Log.js
+++ src/General/Modules/ChangeLog/Log.js
@@ -17,7 +17,8 @@ export const changeLog = [
       "Full support in both Top Gear and Upgrade Finder.",
       "Removed the Vault Domination Gem setting. The panel does the same thing but is much easier to use.",
       "DPS from shards is now included in the Dungeon setting. Further settings will be available in this area soon.",
-      "Re-added a delete button to items."
+      "Re-added a delete button to items.",
+      "Competitive Alternatives from the vault will now have the Vault colour scheme."
     ],
   },
   {
