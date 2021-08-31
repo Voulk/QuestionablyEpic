6d22a555de3d2c5a33a14b4aab7124ad0fb4a0ab
Voulk <itsvoulk@gmail.com>
2021-08-31 13:58:44 -0500

    Minor.

--- src/General/Modules/Player/ClassDefaults/DiscPriestDefaults.js
+++ src/General/Modules/Player/ClassDefaults/DiscPriestDefaults.js
@@ -19,7 +19,7 @@ export const discPriestDefaultSpecialQueries = (contentType) => {
       CastsPerMinute: 36,
       cooldownMult: {
         oneMinute: 1,
-        ninetySeconds: 3.2,
+        ninetySeconds: 3.5,
         twoMinutes: 1,
         twoMinutesOrb: 0.85,
         threeMinutes: 2.5,
