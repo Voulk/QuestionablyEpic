6ac1c5d7324a6dbc55a7080bb6b897a946d2288d
Voulk <itsvoulk@gmail.com>
2021-08-31 09:43:09 -0500

    Minor.

--- src/General/Modules/Player/ClassDefaults/DiscPriestRamps.test.js
+++ src/General/Modules/Player/ClassDefaults/DiscPriestRamps.test.js
@@ -6,27 +6,21 @@ import { runCastSequence, buildRamp } from "./DiscPriestRamps";
 describe("Evang Cast Sequence", () => {
     const player = new Player("Mock", "Discipline Priest", 99, "NA", "Stonemaul", "Night Elf");
     
-    test("Basic Test", () => {
+    const boonSeq = buildRamp('Boon', 10, ["Divine Bell"], player.activeStats.haste, ['Rapture'])
+    const fiendSeq = buildRamp('Fiend', 10, ["Divine Bell"], player.activeStats.haste, ['Rapture'])
+
+    const baselineBoon = runCastSequence(boonSeq, player.activeStats, {"Clarity of Mind": true, "Pelagos": false}, {"Courageous Ascension": 226, "Shining Radiance": 226});
+    const baselineFiend = runCastSequence(fiendSeq, player.activeStats, {"Clarity of Mind": true, "Pelagos": false}, {"Courageous Ascension": 226, "Shining Radiance": 226});
+    
+    const baseline = baselineBoon + baselineFiend;
+
+
+    test("Stat Weights", () => {
 
         //console.log("Boon Ramp with CoM, Bell: " + runCastSequence(demoSequence4, player.activeStats, {"Clarity of Mind": true}, {"Courageous Ascension": 226, "Shining Radiance": 226}));
         //console.log("Boon Ramp with CoM, Bell & Kleia: " + runCastSequence(demoSequence4, player.activeStats, {"Clarity of Mind": true, "Kleia": true}, {"Courageous Ascension": 226}));
-        const boonSeq = buildRamp('Boon', 10, ["Divine Bell"], player.activeStats.haste, ['Rapture'])
-        const fiendSeq = buildRamp('Fiend', 10, ["Divine Bell"], player.activeStats.haste, ['Rapture'])
 
-        console.log(boonSeq);
-        console.log(fiendSeq);
-        const baselineBoon = runCastSequence(boonSeq, player.activeStats, {"Clarity of Mind": true, "Pelagos": false}, {"Courageous Ascension": 226, "Shining Radiance": 226});
-        const baselineFiend = runCastSequence(fiendSeq, player.activeStats, {"Clarity of Mind": true, "Pelagos": false}, {"Courageous Ascension": 226, "Shining Radiance": 226});
         
-        const baseline = baselineBoon + baselineFiend;
-        
-        //console.log("Boon Ramp with CoM, Bell, Radiance & Pelagos (+10 int): " + runCastSequence(demoSequence4, intLine, {"Clarity of Mind": true, "Pelagos": true}, {"Courageous Ascension": 226, "Shining Radiance": 226}));
-        //console.log("Boon Ramp with CoM, Bell, Radiance & Pelagos (+10 crit): " + runCastSequence(demoSequence4, critLine, {"Clarity of Mind": true, "Pelagos": true}, {"Courageous Ascension": 226, "Shining Radiance": 226}));
-        //console.log("Boon Ramp with CoM, Bell, Radiance & Pelagos (+10 vers): " + runCastSequence(demoSequence4, versLine, {"Clarity of Mind": true, "Pelagos": true}, {"Courageous Ascension": 226, "Shining Radiance": 226}));
-        //console.log("Boon Ramp with CoM, Bell, Radiance & Pelagos (+10 mastery): " + runCastSequence(demoSequence4, mastLine, {"Clarity of Mind": true, "Pelagos": true}, {"Courageous Ascension": 226, "Shining Radiance": 226}));
-        //runCastSequence(demoSequence4, player, {"Clarity of Mind": true});
-
-
         // Weights
         
         const stats = ['intellect','versatility', 'crit', 'haste', 'mastery'];
