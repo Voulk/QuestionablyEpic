86d8175720590206f675e88740a94f2d0b3701e5
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-12-06 00:02:19 +1100

    Update PanelLegionTimewalking.js

--- src/General/Modules/UpgradeFinder/Panels/PanelLegionTimewalking.js
+++ src/General/Modules/UpgradeFinder/Panels/PanelLegionTimewalking.js
@@ -1,5 +1,5 @@
 import React from "react";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 import { Typography, Grid, Divider, Paper } from "@mui/material";
 import ItemUpgradeCard from "./ItemUpgradeCard";
 import DungeonHeaderIcons from "../../CooldownPlanner/Functions/IconFunctions/DungeonHeaderIcons";
@@ -27,15 +27,14 @@ export default function LegionTimewalkingGearContainer(props) {
   const gameType = useSelector((state) => state.gameType);
   const contentGenerator = () => {
     return encounterDB[3].map((key, i) => (
-      <Grid item xs={12} key={"LegionTimewalkingContainer-" + i} style={{ padding: "4px 0px" }}>
-        <Paper style={{ backgroundColor: "#191c23", padding: 8, border: "1px solid rgba(255, 255, 255, 0.22)" }}>
-          <Grid container spacing={2}>
-            <Grid item style={{ padding: 0 }}>
+      <Grid item xs={12} key={"LegionTimewalkingContainer-" + i}>
+        <Paper style={{ backgroundColor: "#191c23", border: "1px solid rgba(255, 255, 255, 0.22)" }}>
+          <Grid container>
+            <Grid item xs={12} sm="auto">
               <div
                 style={{
                   width: 207,
                   height: "100%",
-                  paddingLeft: 8,
                   backgroundImage: `url(${DungeonHeaderIcons(key)})`,
                   backgroundRepeat: "no-repeat",
                   backgroundPosition: "center 60%",
@@ -48,8 +47,8 @@ export default function LegionTimewalkingGearContainer(props) {
                 </Typography>
               </div>
             </Grid>
-            <Divider orientation="vertical" flexItem style={{ marginRight: 4 }} />
-            <Grid item xs={12} sm container spacing={1} style={{ marginRight: 6 }}>
+            <Divider orientation="vertical" flexItem />
+            <Grid item xs={12} sm container spacing={1} style={{ padding: 8 }}>
               {[...filterItemListBySource(itemList, -1, key, itemLevels.dungeon[difficulty])].map((item, index) => (
                 <ItemUpgradeCard key={index} item={item} itemDifferential={getDifferentialByID(itemDifferentials, item.id, item.level)} slotPanel={false} />
               ))}
