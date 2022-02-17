79cd88de249fcf1a90a87b05f9e73b6be8c689e4
sabre070 <sabre070@gmail.com>
2022-02-16 19:09:26 -0700

    Fixed 2pc implementation (affects all healing). Updated spell sequence to test for stat priority - notably haste for NL is needed to get more RJW healing within window.

--- src/Retail/Engine/EffectFormulas/Monk/MonkSpellSequence.test.js
+++ src/Retail/Engine/EffectFormulas/Monk/MonkSpellSequence.test.js
@@ -19,6 +19,26 @@ describe("Test Sequences", () => {
         stamina: 1000,
         manaMod: 1,
     }
+    
+    const activeStats2 = {
+        intellect: 2370,
+        haste: 700,
+        crit: 1100,
+        mastery: 85,
+        versatility: 410,
+        stamina: 1000,
+        manaMod: 1,
+    }
+    
+    const activeStats3 = {
+        intellect: 2370,
+        haste: 750,
+        crit: 750,
+        mastery: 85,
+        versatility: 750,
+        stamina: 1000,
+        manaMod: 1,
+    }
 
     let covenants = {"Base": [], "NL": [], "Night Fae": []} 
     const sumValues = obj => Object.values(obj).reduce((a, b) => a + b);
@@ -33,22 +53,34 @@ describe("Test Sequences", () => {
     // ReM just before 4pc extend into second 4pc window
     // 47% chance to have FLS off CD at mentioned cast points.
     const NFsequence = ["Essence Font", "Rising Sun Kick", "Faeline Stomp", "Tiger Palm", "Tiger Palm", "Tiger Palm", "Renewing Mist", "Renewing Mist", "Faeline Stomp", "Thunder Focus Tea", "Refreshing Jade Wind", "Rising Sun Kick", "Chi Burst", "Rising Sun Kick", "Blackout Kick", "Faeline Stomp", "Blackout Kick", "Refreshing Jade Wind", "Essence Font", "Rising Sun Kick", "Tiger Palm", "Tiger Palm", "Tiger Palm", "Blackout Kick", "Refreshing Jade Wind", "Renewing Mist", "Rising Sun Kick"];
+    // 0.8 HPM lost with no reset just before 4pc, not much
+    //const NFsequenceNoFLSReset = ["Essence Font", "Rising Sun Kick", "Faeline Stomp", "Tiger Palm", "Tiger Palm", "Tiger Palm", "Renewing Mist", "Renewing Mist", "Faeline Stomp", "Thunder Focus Tea", "Refreshing Jade Wind", "Rising Sun Kick", "Faeline Stomp", "Rising Sun Kick", "Chi Burst", "Tiger Palm", "Tiger Palm", "Refreshing Jade Wind", "Essence Font", "Rising Sun Kick", "Tiger Palm", "Blackout Kick", "Rising Sun Kick", "Renewing Mist", "Refreshing Jade Wind", "Tiger Palm", "Tiger Palm"];
     const NLsequence = ["Refreshing Jade Wind", "Essence Font", "Tiger Palm", "Tiger Palm", "Tiger Palm", "Renewing Mist", "Renewing Mist", "Bonedust Brew", "Thunder Focus Tea", "Refreshing Jade Wind", "Rising Sun Kick", "Chi Burst", "Rising Sun Kick", "Blackout Kick", "Tiger Palm", "Blackout Kick", "Essence Font", "Refreshing Jade Wind", "Rising Sun Kick", "Tiger Palm", "Tiger Palm", "Tiger Palm", "Blackout Kick", "Renewing Mist", "Rising Sun Kick", "Refreshing Jade Wind"];
     const baseSequence =  ["Refreshing Jade Wind", "Essence Font", "Tiger Palm", "Tiger Palm", "Tiger Palm", "Renewing Mist", "Renewing Mist", "Thunder Focus Tea", "Refreshing Jade Wind", "Rising Sun Kick", "Chi Burst", "Rising Sun Kick", "Blackout Kick", "Tiger Palm", "Blackout Kick", "Essence Font", "Refreshing Jade Wind", "Rising Sun Kick", "Tiger Palm", "Tiger Palm", "Tiger Palm", "Blackout Kick", "Renewing Mist", "Rising Sun Kick", "Refreshing Jade Wind"];
     
     test("Legendaries & Soulbinds", () => {
         
-        covenants["Base"].push(runCastSequence(baseSequence, activeStats, {"DefaultLoadout": true, "covenant": "None", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}))
-        console.log("Base: " + covenants["Base"][0].totalHealing + " (HPM: " + covenants["Base"][0].hpm + "). Damage: " + covenants["Base"][0].totalDamage);
+        covenants["Base"].push(runCastSequence(baseSequence, activeStats3, {"DefaultLoadout": true, "covenant": "None", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}))
+        console.log("Base Balanced Stats: " + covenants["Base"][0].totalHealing + " (HPM: " + covenants["Base"][0].hpm + "). Damage: " + covenants["Base"][0].totalDamage);
 
-        // Multiplied by 1.02 to simulate 2nd legendary
+        // NF 
         covenants["Night Fae"].push(runCastSequence(NFsequence, activeStats, {"DefaultLoadout": true, "covenant": "Night Fae", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
-        console.log("Night Fae: " + covenants["Night Fae"][0].totalHealing*1.02 + " (HPM: " + covenants["Night Fae"][0].hpm*1.02 + "). Damage: " + covenants["Night Fae"][0].totalDamage*1.08);
+        covenants["Night Fae"].push(runCastSequence(NFsequence, activeStats2, {"DefaultLoadout": true, "covenant": "Night Fae", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
+        covenants["Night Fae"].push(runCastSequence(NFsequence, activeStats3, {"DefaultLoadout": true, "covenant": "Night Fae", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
+        console.log("NF Crit/Vers: " + covenants["Night Fae"][0].totalHealing + " (HPM: " + covenants["Night Fae"][0].hpm + "). Damage: " + covenants["Night Fae"][0].totalDamage);
+        console.log("NF Crit/Haste: " + covenants["Night Fae"][1].totalHealing + " (HPM: " + covenants["Night Fae"][1].hpm + "). Damage: " + covenants["Night Fae"][1].totalDamage);
+        console.log("NF Balanced: " + covenants["Night Fae"][2].totalHealing + " (HPM: " + covenants["Night Fae"][2].hpm + "). Damage: " + covenants["Night Fae"][2].totalDamage);
         
         // Multiplied by 1.15 to simulate 2nd legendary
         covenants["NL"].push(runCastSequence(NLsequence, activeStats, {"DefaultLoadout": true, "covenant": "Necrolord", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
         covenants["NL"].push(runCastSequence(baseSequence, activeStats, {"DefaultLoadout": true, "covenant": "Necrolord", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
-        console.log("NL: " + (covenants["NL"][0].totalHealing + covenants["NL"][1].totalHealing*1.15)/2 + " (HPM: " + (covenants["NL"][0].hpm + covenants["NL"][1].hpm*1.15)/2 + "). Damage: " + (covenants["NL"][0].totalDamage+covenants["NL"][1].totalDamage*1.15)/2);
+        covenants["NL"].push(runCastSequence(NLsequence, activeStats2, {"DefaultLoadout": true, "covenant": "Necrolord", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
+        covenants["NL"].push(runCastSequence(baseSequence, activeStats2, {"DefaultLoadout": true, "covenant": "Necrolord", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
+        covenants["NL"].push(runCastSequence(NLsequence, activeStats3, {"DefaultLoadout": true, "covenant": "Necrolord", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
+        covenants["NL"].push(runCastSequence(baseSequence, activeStats3, {"DefaultLoadout": true, "covenant": "Necrolord", "legendaries": ["Ancient Teachings of the Monastery"], "misc": ["2T28", "4T28"]}, {}));
+        console.log("NL Crit/Vers: " + (covenants["NL"][0].totalHealing + covenants["NL"][1].totalHealing*1.15)/2 + " (HPM: " + (covenants["NL"][0].hpm + covenants["NL"][1].hpm*1.15)/2 + "). Damage: " + (covenants["NL"][0].totalDamage+covenants["NL"][1].totalDamage*1.15)/2);
+        console.log("NL Crit/Haste: " + (covenants["NL"][2].totalHealing + covenants["NL"][3].totalHealing*1.15)/2 + " (HPM: " + (covenants["NL"][2].hpm + covenants["NL"][3].hpm*1.15)/2 + "). Damage: " + (covenants["NL"][2].totalDamage+covenants["NL"][3].totalDamage*1.15)/2);
+        console.log("NL Balanced: " + (covenants["NL"][4].totalHealing + covenants["NL"][5].totalHealing*1.15)/2 + " (HPM: " + (covenants["NL"][4].hpm + covenants["NL"][5].hpm*1.15)/2 + "). Damage: " + (covenants["NL"][4].totalDamage+covenants["NL"][5].totalDamage*1.15)/2);
         
     })
 
