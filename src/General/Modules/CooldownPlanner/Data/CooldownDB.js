76e49c8887fd48eed9d86fa72eb02c0c909470ac
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2022-04-04 00:35:04 +1000

    Personals / Potions added

--- src/General/Modules/CooldownPlanner/Data/CooldownDB.js
+++ src/General/Modules/CooldownPlanner/Data/CooldownDB.js
@@ -618,4 +618,36 @@ export const cooldownDB = [
     class: "DeathKnight",
     cdPlannerMenuActive: true,
   },
+
+  {
+    guid: "Potion/Healthstone",
+    name: {
+      en: "Potion/Healthstone",
+      fr: "Potion/Healthstone",
+      de: "Potion/Healthstone",
+      ru: "Potion/Healthstone",
+      cn: "Potion/Healthstone",
+    },
+    icon: require("Images/CooldownPlanner/SpellIcons/trade_alchemy_potionb5.jpg").default,
+    duration: 1,
+    // cooldown: 120,
+    class: "",
+    cdPlannerMenuActive: true,
+  },
+
+  {
+    guid: "Personals",
+    name: {
+      en: "Personals",
+      fr: "Personals",
+      de: "Personals",
+      ru: "Personals",
+      cn: "Personals",
+    },
+    icon: require("Images/CooldownPlanner/SpellIcons/inv_shield_30.jpg").default,
+    duration: 1,
+    // cooldown: 120,
+    class: "",
+    cdPlannerMenuActive: true,
+  },
 ];
