579f758d363b737105e53feb79fc2d4d88005d5d
Voulk <itsvoulk@gmail.com>
2022-04-12 22:46:41 -0500

    Minor

--- src/Retail/Engine/EffectFormulas/Generic/TrinketData.js
+++ src/Retail/Engine/EffectFormulas/Generic/TrinketData.js
@@ -1053,7 +1053,7 @@ export const trinket_data = [
         coefficient: 0.286493,
         table: -7,
         duration: 10,
-        efficiency: {"Restoration Druid": 0.9, "Holy Paladin": 0.75, "Mistweaver Monk": 0, "Restoration Shaman": 0.75, "Holy Priest": 0.76, "Discipline Priest": 0.8},
+        efficiency: {"Restoration Druid": 0.88, "Holy Paladin": 0.75, "Mistweaver Monk": 0, "Restoration Shaman": 0.75, "Holy Priest": 0.75, "Discipline Priest": 0.78},
         stacks: 15, // You start with 20, lose 1 every second and end with 10 for an average of 15.
         ppm: 1,
       },
