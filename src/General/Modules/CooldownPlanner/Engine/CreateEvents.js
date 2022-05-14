501e5f93980b12956d3e8926090de8b6cc0d6f81
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2022-05-15 01:57:48 +1000

    Update CreateEvents.js
    Minor

--- src/General/Modules/CooldownPlanner/Engine/CreateEvents.js
+++ src/General/Modules/CooldownPlanner/Engine/CreateEvents.js
@@ -3,21 +3,61 @@
 export default function createEvents(bossID, difficulty, damageTakenData) {
   console.log(damageTakenData);
   console.log(difficulty);
+  let returnedEvents = [];
 
   const logGuids = damageTakenData.map((key) => key.ability.guid);
+
+  /* ---------------------------------------------------------------------------------------------- */
+  /*                                   Sepulcher of the First Ones                                  */
+  /* ---------------------------------------------------------------------------------------------- */
+
   if (
-    // Jailer
-    bossID === 2537
+    /* ------------------------------------------- Rygelon ------------------------------------------- */
+    bossID === 2549
   ) {
+    /* ---------------------------------------------------------------------------------------------- */
+    /*                                            Abilities                                           */
+    /* ---------------------------------------------------------------------------------------------- */
+    const stellarDecay = 364381;
+
     if (difficulty === "Heroic") {
-      // Surging Azerite
-      damageTakenData.filter((filter) => key.guid === 366408).map((key) => key.guid);
+      // Stellar Decay
+
+      if (logGuids.includes(stellarDecay)) {
+        console.log(damageTakenData.filter((filter) => filter.ability.guid === stellarDecay).map((key) => key.ability.guid));
+      }
     }
+
     if (difficulty === "Mythic") {
-      // Surging Azerite
-      if (logGuids.includes(366408)) {
-        console.log(damageTakenData.filter((filter) => filter.ability.guid === 366408).map((key) => key.ability.guid));
+      // Stellar Decay
+      if (logGuids.includes(stellarDecay)) {
+        console.log(damageTakenData.filter((filter) => filter.ability.guid === stellarDecay).map((key) => key.ability.guid));
       }
     }
   }
+
+  if (
+    /* ------------------------------------------- Jailer ------------------------------------------- */
+    bossID === 2537
+  ) {
+    /* ---------------------------------------------------------------------------------------------- */
+    /*                                            Abilities                                           */
+    /* ---------------------------------------------------------------------------------------------- */
+    const surgingAzerite = 366408;
+    if (difficulty === "Heroic") {
+      // Surging Azerite
+      if (logGuids.includes(surgingAzerite)) {
+        console.log(damageTakenData.filter((filter) => filter.ability.guid === surgingAzerite).map((key) => key.ability.guid));
+      }
+    }
+
+    if (difficulty === "Mythic") {
+      // Surging Azerite
+      if (logGuids.includes(surgingAzerite)) {
+        console.log(damageTakenData.filter((filter) => filter.ability.guid === surgingAzerite).map((key) => key.ability.guid));
+      }
+    }
+  }
+
+  return returnedEvents;
 }
