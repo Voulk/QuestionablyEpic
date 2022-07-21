c69cd238163f9f44f8cd48719d68c5699662b09b
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2022-07-22 00:32:36 +1000

    Why

--- src/General/Modules/SequenceGenerator/SequenceGenerator.js
+++ src/General/Modules/SequenceGenerator/SequenceGenerator.js
@@ -1,210 +1,253 @@
 import React, { useState, useEffect } from "react";
 import ReactGA from "react-ga";
 import { useTranslation } from "react-i18next";
-import { Grid, Button, Typography, Tooltip } from "@mui/material";
+import { Grid, Button, Typography, Tooltip, Paper, Divider } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
 import ArrowForward from "@mui/icons-material/ArrowForward";
 import { getSpellRaw, runCastSequence } from "Retail/Engine/EffectFormulas/Evoker/PresEvokerRamps.js";
-
-import { EVOKERSPELLDB, baseTalents } from "Retail/Engine/EffectFormulas/Evoker/PresEvokerSpellDB"
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import LooksOneIcon from "@mui/icons-material/LooksOne";
+import { EVOKERSPELLDB, baseTalents } from "Retail/Engine/EffectFormulas/Evoker/PresEvokerSpellDB";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      [theme.breakpoints.down("md")]: {
-        margin: "auto",
-        width: "100%",
-        justifyContent: "center",
-        display: "block",
-        marginTop: 140,
-      },
-      [theme.breakpoints.up("sm")]: {
-        margin: "auto",
-        width: "80%",
-        justifyContent: "center",
-        display: "block",
-        marginTop: 120,
-      },
-      [theme.breakpoints.up("md")]: {
-        margin: "auto",
-        width: "65%",
-        justifyContent: "center",
-        display: "block",
-      },
-      [theme.breakpoints.up("lg")]: {
-        margin: "auto",
-        width: "45%",
-        justifyContent: "center",
-        display: "block",
-      },
+  root: {
+    [theme.breakpoints.down("md")]: {
+      margin: "auto",
+      width: "100%",
+      justifyContent: "center",
+      display: "block",
+      marginTop: 140,
     },
-  }));
+    [theme.breakpoints.up("sm")]: {
+      margin: "auto",
+      width: "80%",
+      justifyContent: "center",
+      display: "block",
+      marginTop: 120,
+    },
+    [theme.breakpoints.up("md")]: {
+      margin: "auto",
+      width: "65%",
+      justifyContent: "center",
+      display: "block",
+    },
+    [theme.breakpoints.up("lg")]: {
+      margin: "auto",
+      width: "45%",
+      justifyContent: "center",
+      display: "block",
+    },
+  },
+}));
 
-const dpsSpells = Object.keys(EVOKERSPELLDB).filter(spell => EVOKERSPELLDB[spell][0].type === "damage");
-const healSpells = Object.keys(EVOKERSPELLDB).filter(spell => EVOKERSPELLDB[spell][0].type === "heal" || spell === "Reversion");
-
+const dpsSpells = Object.keys(EVOKERSPELLDB).filter((spell) => EVOKERSPELLDB[spell][0].type === "damage");
+const healSpells = Object.keys(EVOKERSPELLDB).filter((spell) => EVOKERSPELLDB[spell][0].type === "heal" || spell === "Reversion");
 
 export default function SequenceGenerator(props) {
+  const classes = useStyles();
+  const [seq, setSeq] = useState([]);
+  const [talents, settalents] = useState({ ...baseTalents });
+  const [result, setResult] = useState({ totalDamage: 0, totalHealing: 0, hpm: 0 });
 
-    const classes = useStyles();
-    const [seq, setSeq] = useState([]);
-    const [talents, settalents] = useState({...baseTalents});
-    const [result, setResult] = useState({totalDamage: 0, totalHealing: 0, hpm: 0})
+  const stats = {
+    intellect: 2000,
+    haste: 600,
+    crit: 600,
+    mastery: 600,
+    versatility: 600,
+    stamina: 2800,
 
+    critMult: 1,
+  };
 
-    const stats = {
-        intellect: 2000,
-        haste: 600,
-        crit: 600,
-        mastery: 600,
-        versatility: 600,
-        stamina: 2800,
+  const addSpell = (spell) => {
+    setSeq([...seq, spell]);
+    console.log(seq);
+  };
 
-        critMult: 1,
-}
+  const clearSeq = () => {
+    setSeq([]);
+  };
 
-    const addSpell = (spell) => {
+  const runSeq = () => {
+    const sim = runCastSequence(seq, stats, {}, {});
+    setResult(sim);
+    console.log(sim);
+  };
 
-        setSeq([...seq, spell]);
-        console.log(seq);
-    }
-
-    const clearSeq = () => {
-        setSeq([]);
-    }
-
-    const runSeq = () => {
-        const sim = runCastSequence(seq, stats, {}, {});
-        setResult(sim)
-        console.log(sim);
-    }
-
-    return (
-        <div style={{ backgroundColor: "#313131" }}>
-            <div className={classes.root}>
-            <Grid container spacing={1}>
-            <Typography variant="subtitle2" align="left" style={{ width: "100%", padding: "25px 10px 5px 10px" }} color="primary">
-                {"Seq 1"}
+  return (
+    <div style={{ backgroundColor: "#313131" }}>
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center" style={{ padding: "10px 10px 5px 10px" }} color="primary">
+              {/* // TODO Translate */}
+              Sequencer
             </Typography>
-
-            {seq.map((spell, index) => (
-                <Grid item xs={12} sm={12} md={1} lg={1} xl={1} key={index}>
-                <a data-wowhead={"spell=" + EVOKERSPELLDB[spell][0].spellData.id}>
-                <img
-                    height={40}
-                    width={40}
-                    src={ require("Images/Spells/" + EVOKERSPELLDB[spell][0].spellData.icon + ".jpg").default || "" }
-                    alt=""
-                    style={{
-                      borderRadius: 4,
-                      borderWidth: "1px",
-                      borderStyle: "solid",
-                      borderColor: "#008CFF",
-                      marginRight: 0,
-                    }}
-                    />
-                    </a>
-                </Grid>
-            ))}
-
-            <Typography variant="h6" align="left" style={{ width: "100%", padding: "25px 10px 5px 10px" }} color="primary">
-                {"Healing Spells"}
-            </Typography>
-
-            {healSpells.map((spell, index) => (
-                <Grid item xs={12} sm={12} md={1} lg={1} xl={1} key={index}>
-                <a data-wowhead={"spell=" + EVOKERSPELLDB[spell][0].spellData.id}>
-                <img
-                    height={40}
-                    width={40}
-                    src={ require("Images/Spells/" + EVOKERSPELLDB[spell][0].spellData.icon + ".jpg").default || "" }
-                    alt=""
-                    onClick={(e) => addSpell(spell, e)}
-                    style={{
-                      borderRadius: 4,
-                      borderWidth: "1px",
-                      borderStyle: "solid",
-                      borderColor: "#ff8000",
-                      marginRight: 0,
-                    }}
-                    />
-                    </a>
-                </Grid>
-            ))}
-
-            <Typography variant="h6" align="left" style={{ width: "100%", padding: "25px 10px 5px 10px" }} color="primary">
-                {"DPS Spells"}
-            </Typography>
-
-            {dpsSpells.map((spell, index) => (
-                <Grid item xs={12} sm={12} md={1} lg={1} xl={1} key={index}>
-                <a data-wowhead={"spell=" + EVOKERSPELLDB[spell][0].spellData.id}>
-                <img
-                    height={40}
-                    width={40}
-                    src={ require("Images/Spells/" + EVOKERSPELLDB[spell][0].spellData.icon + ".jpg").default || "" }
-                    alt=""
-                    onClick={(e) => addSpell(spell, e)}
-                    style={{
-                      borderRadius: 4,
-                      borderWidth: "1px",
-                      borderStyle: "solid",
-                      borderColor: "#ff8000",
-                      marginRight: 0,
-                    }}
-                    />
-                    </a>
-                </Grid>
-            ))}
-
-            <Typography variant="h6" align="left" style={{ width: "100%", padding: "25px 10px 5px 10px" }} color="primary">
-                {"Talents"}
-            </Typography>
-
-            <Grid item xs={12}>
-            <Button
-              key={321}
-              variant="contained"
-              onClick={() => runSeq()}
-              color="secondary"
-              style={{
-                width: "100%",
-                height: "35px",
-                whiteSpace: "nowrap",
-                justifyContent: "center",
-                textTransform: "none",
-                paddingLeft: "32px",
-                color: "#F2BF59",
-              }}
-            >
-              {"Run Sequence"}
-            </Button>
           </Grid>
           <Grid item xs={12}>
-            <Button
-              key={321}
-              variant="contained"
-              onClick={() => clearSeq()}
-              color="secondary"
-              style={{
-                width: "100%",
-                height: "35px",
-                whiteSpace: "nowrap",
-                justifyContent: "center",
-                textTransform: "none",
-                paddingLeft: "32px",
-                color: "#F2BF59",
-              }}
-            >
-              {"Clear"}
-            </Button>
+            <Paper padding={0} style={{ padding: "10px 5px 10px 10px" }}>
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Typography variant="h6" align="left" style={{ width: "100%" }} color="primary">
+                    {"Sequences"}
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Paper style={{ backgroundColor: "#525252", padding: "8px 8px 4px 8px" }} elevation={0}>
+                    <Grid container spacing={1} alignItems="center">
+                      <Grid item xs="auto">
+                        <LooksOneIcon fontSize="large" />
+                      </Grid>
+
+                      {seq.map((spell, index) => {
+                        return (
+                          <Grid item xs="auto" key={index}>
+                            <a data-wowhead={"spell=" + EVOKERSPELLDB[spell][0].spellData.id}>
+                              <img
+                                height={40}
+                                width={40}
+                                src={require("Images/Spells/" + EVOKERSPELLDB[spell][0].spellData.icon + ".jpg").default || ""}
+                                alt=""
+                                style={{
+                                  borderRadius: 4,
+                                  borderWidth: "1px",
+                                  borderStyle: "solid",
+                                  borderColor: "#008CFF",
+                                  marginRight: 0,
+                                }}
+                              />
+                            </a>
+                          </Grid>
+                        );
+                      })}
+                    </Grid>
+                  </Paper>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Divider />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Typography variant="h6" align="left" style={{ width: "100%" }} color="primary">
+                    {"Healing Spells"}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Grid container spacing={1}>
+                    {healSpells.map((spell, index) => (
+                      <Grid item xs="auto" key={index}>
+                        <a data-wowhead={"spell=" + EVOKERSPELLDB[spell][0].spellData.id}>
+                          <img
+                            height={40}
+                            width={40}
+                            src={require("Images/Spells/" + EVOKERSPELLDB[spell][0].spellData.icon + ".jpg").default || ""}
+                            alt=""
+                            onClick={(e) => addSpell(spell, e)}
+                            style={{
+                              borderRadius: 4,
+                              borderWidth: "1px",
+                              borderStyle: "solid",
+                              borderColor: "#ff8000",
+                              marginRight: 0,
+                            }}
+                          />
+                        </a>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Typography variant="h6" align="left" style={{ width: "100%" }} color="primary">
+                    {"DPS Spells"}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Grid container spacing={1}>
+                    {dpsSpells.map((spell, index) => (
+                      <Grid item xs="auto" key={index}>
+                        <a data-wowhead={"spell=" + EVOKERSPELLDB[spell][0].spellData.id}>
+                          <img
+                            height={40}
+                            width={40}
+                            src={require("Images/Spells/" + EVOKERSPELLDB[spell][0].spellData.icon + ".jpg").default || ""}
+                            alt=""
+                            onClick={(e) => addSpell(spell, e)}
+                            style={{
+                              borderRadius: 4,
+                              borderWidth: "1px",
+                              borderStyle: "solid",
+                              borderColor: "#ff8000",
+                              marginRight: 0,
+                            }}
+                          />
+                        </a>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Typography variant="h6" align="left" style={{ width: "100%" }} color="primary">
+                    {"Talents"}
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button
+                    key={321}
+                    variant="contained"
+                    onClick={() => runSeq()}
+                    color="secondary"
+                    style={{
+                      width: "100%",
+                      height: "35px",
+                      whiteSpace: "nowrap",
+                      justifyContent: "center",
+                      textTransform: "none",
+                      paddingLeft: "32px",
+                      color: "#F2BF59",
+                    }}
+                  >
+                    {"Run Sequence"}
+                  </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    key={321}
+                    variant="contained"
+                    onClick={() => clearSeq()}
+                    color="secondary"
+                    style={{
+                      width: "100%",
+                      height: "35px",
+                      whiteSpace: "nowrap",
+                      justifyContent: "center",
+                      textTransform: "none",
+                      paddingLeft: "32px",
+                      color: "#F2BF59",
+                    }}
+                  >
+                    {"Clear"}
+                  </Button>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Typography variant="h6" align="left" style={{ width: "100%" }} color="primary">
+                    {"Results"}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Paper style={{ backgroundColor: "#525252", padding: 16 }} elevation={0}>
+                    <p style={{ color: "whitesmoke", paddingTop: "10px" }}>{"Damage: " + result.totalDamage + ". Healing: " + result.totalHealing + ". HPM: " + Math.round(100 * result.hpm) / 100}</p>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
-
-        <p style={{ color: "whitesmoke", paddingTop: "10px" }}>{"Damage: " + result.totalDamage + ". Healing: " + result.totalHealing + ". HPM: " + Math.round(100*result.hpm)/100}</p>
-
         </Grid>
-            </div>
-        </div>
-    )
-
-
+      </div>
+    </div>
+  );
 }
