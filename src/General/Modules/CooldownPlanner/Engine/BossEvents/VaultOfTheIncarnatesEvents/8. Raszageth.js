c841ed34ddf24118449900f4f171a85f36779b9c
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2022-12-21 03:11:16 +1100

    Defaults + WIP Rasz

--- src/General/Modules/CooldownPlanner/Engine/BossEvents/VaultOfTheIncarnatesEvents/8. Raszageth.js
+++ src/General/Modules/CooldownPlanner/Engine/BossEvents/VaultOfTheIncarnatesEvents/8. Raszageth.js
@@ -9,5 +9,33 @@ export default function createRaszagethEvents(bossID, difficulty, damageTakenDat
     .concat(debuffs.map((key) => key.ability.guid))
     .concat(buffData.map((key) => key.ability.guid));
 
+  // const enemyData = enemyHealth["series"].filter((filter) => filter.guid === 189492);
+  // const enemyHealthData = Object.entries(enemyData[0]["data"]).map((key) => {
+  //   return {
+  //     time: key[1][0],
+  //     health: key[1][1],
+  //   };
+  // });
+
+  // events.push({ time: "00:00", bossAbility: "Phase 1" });
+
+  // const intermissionOne = enemyHealthData.filter((filter) => filter.health <= 65)[0];
+  // intermissionOne !== undefined
+  //   ? events.push({
+  //       time: moment.utc(fightDuration(intermissionOne.time, starttime)).startOf("second").format("mm:ss"),
+  //       bossAbility: "Intermission",
+  //     })
+  //   : "";
+  // const phase3 = enemyHealthData.filter((filter) => filter.health <= 15)[0];
+  // phase3 !== undefined
+  //   ? events.push({
+  //       time: moment
+  //         .utc(fightDuration(phase3.time - 1000, starttime))
+  //         .startOf("second")
+  //         .format("mm:ss"),
+  //       bossAbility: "Phase 3",
+  //     })
+  //   : "";
+
   return events;
 }
