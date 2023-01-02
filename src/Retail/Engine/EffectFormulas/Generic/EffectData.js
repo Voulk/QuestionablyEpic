9677165f003e82e48481950c1f30954f9340ade8
Voulk <itsvoulk@gmail.com>
2023-01-02 13:09:15 -0600

    Added Assembly Guardian's Ring effect, log.

--- src/Retail/Engine/EffectFormulas/Generic/EffectData.js
+++ src/Retail/Engine/EffectFormulas/Generic/EffectData.js
@@ -26,6 +26,29 @@ export const effectData = [
   },
   {
     /* -------------------- */
+    /* Assembly Guardian's Band                      
+    /* -------------------- */
+    /* This is the "Tank" version of the ring. It's a self-absorb but is tuned a little higher than the heal ring.
+    */
+    name: "Assembly Guardian's Ring",
+    effects: [
+      { 
+        coefficient: 183.679,
+        table: -9,
+        efficiency: {Raid: 0.7, Dungeon: 0.45},
+        ppm: 2,
+        secondaries: ['versatility'],
+      },
+    ],
+    runFunc: function(data, player, itemLevel, additionalData) {
+      let bonus_stats = {};
+      bonus_stats.hps = processedValue(data[0], itemLevel, data[0].efficiency) * player.getStatMults(data[0].secondaries) * data[0].ppm / 60;
+
+      return bonus_stats;
+    }
+  },
+  {
+    /* -------------------- */
     /* Drakebreaker's Versatility                       
     /* -------------------- */
     /* Flat 105 Versatility. We don't need to do anything fancy for it, just return 105 vers.
