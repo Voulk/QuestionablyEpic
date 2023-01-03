301935ad427ed25018c101f9e05089e6d8a37bfa
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2023-01-04 00:56:24 +1100

    ERT fix? Snack Fix Emerald Communion Added

--- src/General/Modules/CooldownPlanner/ModuleComponents/Engine/ERTEngine.js
+++ src/General/Modules/CooldownPlanner/ModuleComponents/Engine/ERTEngine.js
@@ -15,7 +15,8 @@ const debug = false;
 export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCooldownsChecked) {
   debug && console.log(" -- Debugging On -> ERTEngine.js --");
   const seperator = " - ";
-  const space = "  ";
+  const space = " ";
+  const doubleSpace = "  ";
 
   const abilityArr = [
     ...bossAbilities[bossID]
@@ -57,58 +58,72 @@ export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCoo
           ? key.cooldown0 === "" || key.cooldown0 === undefined
             ? ""
             : key.cooldown0 === "Personals"
-            ? "{everyone}Personals" + "{spell:160955}"
+            ? "{everyone}Personals" + space + "{spell:160955}"
             : key.cooldown0 === "Potion/Healthstone"
-            ? "{everyone}Potion/Healthstone" + "{spell:105683}"
+            ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : classColoursERT(key.class0) + key.name0 + "|r" + "{spell:" + key.cooldown0 + "}";
+          : classColoursERT(key.class0) + key.name0 + "|r" + space + "{spell:" + key.cooldown0 + "}";
 
       let option1 =
         key.name1 === "" || key.name1 === undefined
           ? key.cooldown1 === "" || key.cooldown1 === undefined
             ? ""
             : key.cooldown1 === "Personals"
-            ? space + "{everyone}Personals" + "{spell:160955}"
+            ? "{everyone}Personals" + space + "{spell:160955}"
             : key.cooldown1 === "Potion/Healthstone"
-            ? space + "{everyone}Potion/Healthstone" + "{spell:105683}"
+            ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : space + classColoursERT(key.class1) + key.name1 + "|r" + "{spell:" + key.cooldown1 + "}";
+          : classColoursERT(key.class1) + key.name1 + "|r" + space + "{spell:" + key.cooldown1 + "}";
 
       let option2 =
         key.name2 === "" || key.name2 === undefined
           ? key.cooldown2 === "" || key.cooldown2 === undefined
             ? ""
             : key.cooldown2 === "Personals"
-            ? space + "{everyone}Personals" + "{spell:160955}"
+            ? "{everyone}Personals" + space + "{spell:160955}"
             : key.cooldown2 === "Potion/Healthstone"
-            ? space + "{everyone}Potion/Healthstone" + "{spell:105683}"
+            ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : space + classColoursERT(key.class2) + key.name2 + "|r" + "{spell:" + key.cooldown2 + "}";
+          : classColoursERT(key.class2) + key.name2 + "|r" + space + "{spell:" + key.cooldown2 + "}";
 
       let option3 =
         key.name3 === "" || key.name3 === undefined
           ? key.cooldown3 === "" || key.cooldown3 === undefined
             ? ""
             : key.cooldown3 === "Personals"
-            ? space + "{everyone}Personals" + "{spell:160955}"
+            ? "{everyone}Personals" + space + "{spell:160955}"
             : key.cooldown3 === "Potion/Healthstone"
-            ? space + "{everyone}Potion/Healthstone" + "{spell:105683}"
+            ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : space + classColoursERT(key.class3) + key.name3 + "|r" + "{spell:" + key.cooldown3 + "}";
+          : classColoursERT(key.class3) + key.name3 + "|r" + space + "{spell:" + key.cooldown3 + "}";
 
       let option4 =
         key.name4 === "" || key.name4 === undefined
           ? key.cooldown4 === "" || key.cooldown4 === undefined
             ? ""
             : key.cooldown4 === "Personals"
-            ? space + "{everyone}Personals" + "{spell:160955}"
+            ? "{everyone}Personals" + space + "{spell:160955}"
             : key.cooldown4 === "Potion/Healthstone"
-            ? space + "{everyone}Potion/Healthstone" + "{spell:105683}"
+            ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : space + classColoursERT(key.class4) + key.name4 + "|r" + "{spell:" + key.cooldown4 + "}";
+          : classColoursERT(key.class4) + key.name4 + "|r" + space + "{spell:" + key.cooldown4 + "}";
 
+      console.log(option0);
       return {
-        ert: time + (key.bossAbility === undefined ? "" : translatedName + seperator) + option0 + option1 + option2 + option3 + option4,
+        ert:
+          time +
+          seperator +
+          (key.bossAbility === undefined ? "" : translatedName + seperator) +
+          (key.bossAbility === undefined ? "" : space) +
+          option0 +
+          (option0 === "" ? "" : doubleSpace) +
+          option1 +
+          (option1 === "" ? "" : doubleSpace) +
+          option2 +
+          (option2 === "" ? "" : doubleSpace) +
+          option3 +
+          (option3 === "" ? "" : doubleSpace) +
+          option4,
         /* --------------------------------- This is for Sorting by Time -------------------------------- */
         time: key.time,
       };
