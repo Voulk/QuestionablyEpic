34ca83d08677efe299c38df31dc8ce636024376a
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2023-01-23 05:49:29 +1100

    Custom Entry for cooldown columns + ERT update/test

--- src/General/Modules/CooldownPlanner/ModuleComponents/Engine/ERTEngine.js
+++ src/General/Modules/CooldownPlanner/ModuleComponents/Engine/ERTEngine.js
@@ -13,6 +13,11 @@ import { bossAbilities } from "../../Data/CooldownPlannerBossAbilityList";
 const debug = false;
 
 export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCooldownsChecked) {
+  console.log(tableData);
+  console.log(bossID);
+  console.log(lang);
+  console.log(hideNoCooldownsChecked);
+
   debug && console.log(" -- Debugging On -> ERTEngine.js --");
   const seperator = " - ";
   const space = " ";
@@ -53,27 +58,39 @@ export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCoo
             .toString()
         : key.bossAbility;
 
+      const checkCooldown = (cooldown) => {
+        if (typeof cooldown === "number") {
+          return "{spell:" + cooldown + "}";
+        } else {
+          return cooldown;
+        }
+      };
+
       let option0 =
-        key.name0 === "" || key.name0 === undefined
-          ? key.cooldown0 === "" || key.cooldown0 === undefined
+        key.cooldown0 === "" || key.cooldown0 === undefined
+          ? key.name0 === "" || key.name0 === undefined
             ? ""
             : key.cooldown0 === "Personals"
             ? "{everyone}Personals" + space + "{spell:160955}"
             : key.cooldown0 === "Potion/Healthstone"
             ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : classColoursERT(key.class0) + key.name0 + "|r" + space + "{spell:" + key.cooldown0 + "}";
+          : (key.class0 === "" || key.class0 === undefined ? "" : classColoursERT(key.class0)) +
+            (key.name0 === "" || key.name0 === undefined ? "" : key.name0 + "|r" + space) +
+            checkCooldown(key.cooldown0);
 
       let option1 =
-        key.name1 === "" || key.name1 === undefined
-          ? key.cooldown1 === "" || key.cooldown1 === undefined
+        key.cooldown1 === "" || key.cooldown1 === undefined
+          ? key.name1 === "" || key.name1 === undefined
             ? ""
             : key.cooldown1 === "Personals"
             ? "{everyone}Personals" + space + "{spell:160955}"
             : key.cooldown1 === "Potion/Healthstone"
             ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : classColoursERT(key.class1) + key.name1 + "|r" + space + "{spell:" + key.cooldown1 + "}";
+          : (key.class1 === "" || key.class1 === undefined ? "" : classColoursERT(key.class1)) +
+            (key.name1 === "" || key.name1 === undefined ? "" : key.name1 + "|r" + space) +
+            checkCooldown(key.cooldown1);
 
       let option2 =
         key.name2 === "" || key.name2 === undefined
@@ -84,7 +101,9 @@ export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCoo
             : key.cooldown2 === "Potion/Healthstone"
             ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : classColoursERT(key.class2) + key.name2 + "|r" + space + "{spell:" + key.cooldown2 + "}";
+          : (key.class2 === "" || key.class2 === undefined ? "" : classColoursERT(key.class2)) +
+            (key.name2 === "" || key.name2 === undefined ? "" : key.name2 + "|r" + space) +
+            checkCooldown(key.cooldown2);
 
       let option3 =
         key.name3 === "" || key.name3 === undefined
@@ -95,7 +114,9 @@ export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCoo
             : key.cooldown3 === "Potion/Healthstone"
             ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : classColoursERT(key.class3) + key.name3 + "|r" + space + "{spell:" + key.cooldown3 + "}";
+          : (key.class3 === "" || key.class3 === undefined ? "" : classColoursERT(key.class3)) +
+            (key.name3 === "" || key.name3 === undefined ? "" : key.name3 + "|r" + space) +
+            checkCooldown(key.cooldown3);
 
       let option4 =
         key.name4 === "" || key.name4 === undefined
@@ -106,7 +127,9 @@ export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCoo
             : key.cooldown4 === "Potion/Healthstone"
             ? "{everyone}Potion/Healthstone" + space + "{spell:105683}"
             : ""
-          : classColoursERT(key.class4) + key.name4 + "|r" + space + "{spell:" + key.cooldown4 + "}";
+          : (key.class4 === "" || key.class4 === undefined ? "" : classColoursERT(key.class4)) +
+            (key.name4 === "" || key.name4 === undefined ? "" : key.name4 + "|r" + space) +
+            checkCooldown(key.cooldown4);
 
       return {
         ert:
@@ -128,6 +151,7 @@ export default function ertEngine(tableData, bossID, lang, setERTData, hideNoCoo
       };
     });
   // debug && console.table(ertNoteTimeIcons);
+  debug && console.log(ertNoteTimeIcons);
 
   setERTData(ertNoteTimeIcons);
 }
