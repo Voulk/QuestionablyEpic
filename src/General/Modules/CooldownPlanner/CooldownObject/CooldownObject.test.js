a8aee30606a545489e01080867b5072ad8574fc2
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2023-01-27 01:06:32 +1100

    Delete CooldownObject.test.js

--- src/General/Modules/CooldownPlanner/CooldownObject/CooldownObject.test.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Cooldowns from "../CooldownObject/CooldownObject";
-
-const cooldownObject = new Cooldowns();
-
-// removed old replace names function, new findAndReplace should work for names as well as classes
