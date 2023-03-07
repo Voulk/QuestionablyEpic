978a6a51dd03a310612dedc94421d92feae4dc73
Voulk <itsvoulk@gmail.com>
2023-03-06 22:37:18 -0600

    Druid spell data.

--- src/General/Modules/OneShot/ClassData.js
+++ src/General/Modules/OneShot/ClassData.js
@@ -2,15 +2,19 @@
 
 export const specData = {
     "druid": {
-
+        "passives": ["Thick Hide"],
+        "defensives": ["Barkskin", "Ironfur"]
     },
     "restoration druid": {
     },
     "balance druid": {
     },
     "feral druid": {
+        "defensives": ["Survival Instincts", "Protective Growth"]
     },
     "guardian druid": {
+        "passives": ["Reinforced Fur", "Scintillating Moonlight", "Rend and Tear", "Rage of the Sleeper"],
+        "defensives": ["Survival Instincts", "Pulverize", "Incarnation: Guardian of Ursoc", ]
     },
     "evoker": {
         "passives": ["Inherent Resistance"],
@@ -19,6 +23,10 @@ export const specData = {
     "preservation evoker": {
     },
     "devastation evoker": {
+    },
+    "monk": {
+        "passives": [],
+        "defensives": ["Dampen Harm", "Fortifying Brew", "Diffuse Magic"]
     }
 
 }
