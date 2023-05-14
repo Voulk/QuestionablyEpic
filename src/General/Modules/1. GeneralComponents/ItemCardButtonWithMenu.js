b3e3de97f4603e545ad438c0ca48c303ba54022e
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2023-05-14 23:24:02 +0000

    Add ItemCardButtonWithMenu

---
+++ src/General/Modules/1. GeneralComponents/ItemCardButtonWithMenu.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
+
+export default function ItemCardButtonWithMenu() {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const menuItems = [
+    { id: 1, label: 'Menu Item 1' },
+    { id: 2, label: 'Menu Item 2' },
+    { id: 3, label: 'Menu Item 3' },
+  ];
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  // Function to execute when menu item is clicked
+  const handleMenuItemClick = (menuItem) => {
+    console.log(`Menu item ${menuItem.label} clicked`);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <IconButton color="primary" onClick={handleClick}>
+        <CompareArrowsIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {menuItems.map((item) => (
+          <MenuItem key={item.id} onClick={() => handleMenuItemClick(item)}>
+            {item.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
