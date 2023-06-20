92dcf0b6533e76d65678709c238a77fee42ca81c
Voulk <itsvoulk@gmail.com>
2023-06-20 12:31:09 -0500

    Updated index.tsx to rewrite our createStore.

--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,13 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { createRoot } from 'react-dom/client';
 import rootReducer from "./Redux/Reducers/RootReducer";
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 
-const store = createStore(rootReducer, /* preloadedState, */ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, /* preloadedState, */ composeWithDevTools());
 
 
-const container = document.getElementById('root');
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 const render = () => {
   root.render(
