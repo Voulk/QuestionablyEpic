3d3b4ce34fed49c654f4ac57402d1b820ae04814
Voulk <itsvoulk@gmail.com>
2023-12-01 21:59:36 -0600

    MotD

--- src/General/Modules/SetupAndMenus/MessageOftheDay.tsx
+++ src/General/Modules/SetupAndMenus/MessageOftheDay.tsx
@@ -3,7 +3,7 @@ import { Grid, Paper, Typography } from "@mui/material";
 export default function MessageOfTheDay() {
   //prettier-ignore
   const messageOfTheDay: string[] = [
-      "-> Expect changes through the week. Amirdrassil tier sets will be in with season release.",
+      "-> Blizzard continue to make both class and item changes.",
   ]
 ;
 
