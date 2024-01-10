70c26d6100519b94459350efdfebec13c1420f7b
Voulk <itsvoulk@gmail.com>
2024-01-10 11:10:06 -0600

    Added all Ulduar mana and stat proc trinkets.

--- src/Retail/Engine/EffectFormulas/Generic/Trinkets/TimewalkingTrinketData.ts
+++ src/Retail/Engine/EffectFormulas/Generic/Trinkets/TimewalkingTrinketData.ts
@@ -35,6 +35,110 @@ export const timewalkingTrinketData = [
             return bonus_stats;
         }
       },
+      {
+        /* ---------------------------------------------------------------------------------------------- */
+        /*                                    Spark of Hope                                     */
+        /* ---------------------------------------------------------------------------------------------- */
+        /* 
+        */
+        name: "Spark of Hope",
+        effects: [
+          { // Mastery portion
+            coefficient: 0.450353,
+            table: -7,
+            ppm: {"Restoration Druid": 35, "Holy Priest": 14, "Restoration Shaman": 12, "Holy Paladin": 10, "Mistweaver Monk": 12, 
+                  "Preservation Evoker": 6, "Discipline Priest": 9} // Relevant casts per minute. Can auto-pull from logs.
+          },
+        ],
+        runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+          let bonus_stats: Stats = {};
+          bonus_stats.mana = data[0].ppm[player.spec] * processedValue(data[0], itemLevel) / 60;
+          return bonus_stats;
+        }
+      },
+      {
+        /* ---------------------------------------------------------------------------------------------- */
+        /*                                      Show of Faith                                          */
+        /* ---------------------------------------------------------------------------------------------- */
+        /* 
+        */
+        name: "Show of Faith",
+        effects: [
+          { // 
+            coefficient: 1.595796,
+            table: -7,
+            ppm: 2
+          },
+        ],
+        runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+          let bonus_stats: Stats = {};
+          bonus_stats.mana = data[0].ppm * processedValue(data[0], itemLevel) / 60;
+          return bonus_stats;
+        }
+      },
+      {
+        /* ---------------------------------------------------------------------------------------------- */
+        /*                                      Sif's Remembrance                                          */
+        /* ---------------------------------------------------------------------------------------------- */
+        /* 
+        */
+        name: "Sif's Remembrance",
+        effects: [
+          { // 
+            coefficient: 1.125146,
+            duration: 15,
+            table: -1,
+            ppm: 2
+          },
+        ],
+        runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+          let bonus_stats: Stats = {};
+          bonus_stats.intellect = runGenericPPMTrinket(data[0], itemLevel);
+          return bonus_stats;
+        }
+      },
+      {
+        /* ---------------------------------------------------------------------------------------------- */
+        /*                                        Pandora's Plea                                          */
+        /* ---------------------------------------------------------------------------------------------- */
+        /* 
+        */
+        name: "Pandora's Plea",
+        effects: [
+          { // 
+            coefficient: 1.561615,
+            duration: 10,
+            table: -1,
+            ppm: 2
+          },
+        ],
+        runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+          let bonus_stats: Stats = {};
+          bonus_stats.intellect = runGenericPPMTrinket(data[0], itemLevel);
+          return bonus_stats;
+        }
+      },
+      {
+        /* ---------------------------------------------------------------------------------------------- */
+        /*                                        Eye of the Broodmother                                          */
+        /* ---------------------------------------------------------------------------------------------- */
+        /* 100% uptime on full stacks since it procs off everything.
+        */
+        name: "Eye of the Broodmother",
+        effects: [
+          { // 
+            coefficient: 0.10503,
+            duration: 10,
+            stacks: 5,
+            table: -1,
+          },
+        ],
+        runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+          let bonus_stats: Stats = {};
+          bonus_stats.intellect = processedValue(data[0], itemLevel) * data[0].stacks!;
+          return bonus_stats;
+        }
+      },
 
 
 ]
