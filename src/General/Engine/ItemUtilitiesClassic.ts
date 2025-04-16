366c6b367440f90fadb861c93fc2d8fede3fa474
Voulk <itsvoulk@gmail.com>
2025-04-16 13:04:27 -0500

    Converted ItemUtilitiesClassic to ts

--- src/General/Engine/ItemUtilitiesClassic.ts
+++ src/General/Engine/ItemUtilitiesClassic.ts
@@ -1,6 +1,6 @@
 
 
-export function filterClassicItemListBySource(itemList, sourceInstance, sourceBoss) {
+export function filterClassicItemListBySource(itemList: any[], sourceInstance: number, sourceBoss: number) {
     let temp = itemList.filter(function (item) {
       return (item.source.instanceId == sourceInstance && item.source.encounterId == sourceBoss) || (item.source.instanceId == sourceInstance && sourceBoss == 0);
     });
