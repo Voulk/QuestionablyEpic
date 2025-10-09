303ae92471bfac6b98fbbb69003c4d4a1580d6b3
Voulk <itsvoulk@gmail.com>
2025-10-09 17:52:49 -0500

    Fixed Disc reforge order

--- src/General/Modules/Player/ClassDefaults/Classic/Priest/DisciplinePriestClassic.js
+++ src/General/Modules/Player/ClassDefaults/Classic/Priest/DisciplinePriestClassic.js
@@ -41,7 +41,7 @@ export const discPriestDefaults = {
     specialQueries: {
         // Any special information we need to pull.
     },
-    autoReforgeOrder: ["crit", "spirit", "mastery", "haste", "hit"],
+    autoReforgeOrder: ["crit", "mastery", "spirit", "haste", "hit"],
 
 }
 
