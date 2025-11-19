3ce890683ce6903f69d8248ad322f79767340f73
Voulk <itsvoulk@gmail.com>
2025-11-18 23:18:33 -0600

    First talentData implementation

---
+++ src/General/Modules/Player/ClassDefaults/RestoShaman/RestoShamanTalents.ts
@@ -0,0 +1,21 @@
+
+
+const classTalents: TalentTree = {
+
+}
+
+const specTalents: TalentTree = {
+
+}
+
+const heroTalents: TalentTree = {
+    // Farseer
+
+    // Totemic
+}
+
+export const druidTalents = {
+    ...classTalents,
+    ...specTalents,
+    ...heroTalents,
+};
