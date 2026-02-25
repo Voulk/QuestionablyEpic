2c77c10597aae90100a48c0bde209c8a5f87ead1
Voulk <itsvoulk@gmail.com>
2026-02-25 15:53:57 -0600

    Added ilvls to WQ / Delve trinkets

--- src/Retail/Engine/EffectFormulas/Generic/Trinkets/OtherTrinketData.ts
+++ src/Retail/Engine/EffectFormulas/Generic/Trinkets/OtherTrinketData.ts
@@ -1,7 +1,6 @@
-import { convertPPMToUptime, runGenericFlatProc, getSetting, processedValue, runGenericPPMTrinket, forceGenericOnUseTrinket, runGenericOnUseTrinket, getDiminishedValue, buildIdolTrinket, runGenericRandomPPMTrinket } from "Retail/Engine/EffectFormulas/EffectUtilities";
+import { convertPPMToUptime, runGenericFlatProc, getSetting, processedValue, runGenericPPMTrinket, getHighestStat, forceGenericOnUseTrinket, runGenericOnUseTrinket, getDiminishedValue, buildIdolTrinket, runGenericRandomPPMTrinket } from "Retail/Engine/EffectFormulas/EffectUtilities";
 import { Player } from "General/Modules/Player/Player";
-import { randPropPoints } from "Retail/Engine/RandPropPointsBylevel";
-import { combat_ratings_mult_by_ilvl } from "Retail/Engine/CombatMultByLevel";
+import trinketRawData from "Retail/Engine/EffectFormulas/Generic/Trinkets/TrinketData.json"
 
 export const otherTrinketData = [
       { 
@@ -20,11 +19,75 @@ export const otherTrinketData = [
     runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
       let bonus_stats: Stats = {};
 
-      bonus_stats.intellect = runGenericPPMTrinket(data[0], itemLevel);
+      bonus_stats.intellect = runGenericPPMTrinket({...data[0], ...trinketRawData["Magister's Alchemist Stone"][0]}, itemLevel);
 
       return bonus_stats;
     }
   },
+  {
+      name: "Glorious Crusader's Keepsake",
+      description: "Not available on Myth track but quite good as an early trinket - regardless of whether others are also using it.",
+      addonDescription: "Not available on Myth track but quite good as an early trinket - regardless of whether others are also using it.",
+      effects: [
+        {
+          duration: 15,
+          ppm: 2,
+          stat: "highest",
+        },
+        { // This only fires if you have other people wearing it.
+          duration: 15,
+          ppm: 2,
+          stat: "allyStats",
+        },
+      ],
+      runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+        let bonus_stats = {};
+
+        //console.log(JSON.stringify(additionalData.selectedOptions));
+
+        const bestStat = getHighestStat(additionalData.setStats);
+        bonus_stats[bestStat] = runGenericPPMTrinket({...data[0], ...trinketRawData["Glorious Crusader's Keepsake"][0], stat: bestStat}, itemLevel, player);
+        //bonus_stats.allyStats = runGenericPPMTrinket({...data[1], ...trinketRawData["Glorious Crusader's Keepsake"][1]}, itemLevel, player);
+  
+        return bonus_stats;
+      }
+  },
+      {
+        name: "Cosmic Bell",
+        description: "Does not heal for anywhere CLOSE to enough to be more than bottom tier. ",
+        addonDescription: "Does not heal for anywhere CLOSE to enough to be more than bottom tier. ",
+        effects: [
+          { 
+            secondaries: ['versatility', 'crit'],
+            cooldown: 150,
+            efficiency: {Raid: 0.7, Dungeon: 0.55} //
+          },
+        ],
+        runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+          let bonus_stats: Stats = {};
+    
+          bonus_stats.hps = runGenericFlatProc({...data[0], ...trinketRawData["Cosmic Bell"][0]}, itemLevel, player, additionalData.contentType)
+          return bonus_stats;
+        }
+      },
+    {
+        name: "Ultradon Cuirass",
+        description: "A legitimately huge absorb that you could find niche use for, but not generally useful. ",
+        addonDescription: "A legitimately huge absorb that you could find niche use for, but not generally useful. ",
+        effects: [
+          { 
+            secondaries: ['versatility'],
+            cooldown: 150,
+            efficiency: {Raid: 0.7, Dungeon: 0.55} //
+          },
+        ],
+        runFunc: function(data: Array<effectData>, player: Player, itemLevel: number, additionalData: any) {
+          let bonus_stats: Stats = {};
+    
+          bonus_stats.hps = runGenericFlatProc({...data[0], ...trinketRawData["Ultradon Cuirass"][0]}, itemLevel, player, additionalData.contentType)
+          return bonus_stats;
+        }
+      },
 
   
 
