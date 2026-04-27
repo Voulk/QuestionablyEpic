ded09744a57b6b93eeb3dd0c4029c2030c91dfb7
William Lang <264728+williamlang@users.noreply.github.com>
2026-04-27 14:53:52 -0700

    remvoe spell data accordion as its not needed

--- src/General/Modules/TopGear/Report/SpellDataAccordion.tsx
+++ src/General/Modules/TopGear/Report/SpellDataAccordion.tsx
@@ -1,28 +1,16 @@
 import React from "react";
-import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
-  Box,
-  Grid,
-} from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Typography } from "@mui/material";
 import SequenceDataTable from "General/Modules/SequenceGenerator/SequenceDataTable";
 import ErrorBoundary from "General/SystemTools/ErrorLogging/ErrorBoundary";
 
 interface SpellDataAccordionProps {
   spec: string;
-  statList: any; 
+  statList: any;
   data?: string;
   talents?: any;
   title?: string;
   description?: string;
-  defaultExpanded?: boolean;
 }
-
-
-// The spell data component
 
 const SpellDataAccordion: React.FC<SpellDataAccordionProps> = ({
   spec,
@@ -30,45 +18,17 @@ const SpellDataAccordion: React.FC<SpellDataAccordionProps> = ({
   talents = null,
   title = "Spell Data",
   description = "This is an advanced panel that shows you spell information based on what gear your top set above is wearing. You can use it to make determinations on which spells to cast.",
-  defaultExpanded = true,
 }) => {
   return (
-    <Grid item xs={12} style={{ paddingBottom: "10px" }}>
-      <Accordion
-        defaultExpanded={defaultExpanded}
-        sx={{
-          backgroundColor: "transparent",
-          backgroundImage: "none",
-          boxShadow: "none",
-        }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="sequence-panel-content"
-          id="sequence-panel-header"
-          sx={{
-             minHeight: 56,
-          }}
-        >
-          <Box>
-            <Typography variant="h5" sx={{ paddingTop: "20px" }}>{title}</Typography>
-          </Box>
-        </AccordionSummary>
-
-        <AccordionDetails>
-          <Typography variant="body2" gutterBottom>
-            {description}
-          </Typography>
-          <ErrorBoundary>
-            <SequenceDataTable
-              spec={spec}
-              stats={statList}
-              talents={talents}
-            />
-          </ErrorBoundary>
-        </AccordionDetails>
-      </Accordion>
-    </Grid>
+    <div>
+      <Typography variant="h5">{title}</Typography>
+      <Typography variant="body2" sx={{ paddingBottom: "4px" }} gutterBottom>
+        {description}
+      </Typography>
+      <ErrorBoundary>
+        <SequenceDataTable spec={spec} stats={statList} talents={talents} />
+      </ErrorBoundary>
+    </div>
   );
 };
 
