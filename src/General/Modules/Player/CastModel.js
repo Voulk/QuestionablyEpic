31beedb19fe96bbb872cf29fd3008da6299c57d5
Voulk <itsvoulk@gmail.com>
2026-09-17 18:30:40 -0500

    Fixed some missing model types. Added more tooltip information to Whelp.

--- src/General/Modules/Player/CastModel.js
+++ src/General/Modules/Player/CastModel.js
@@ -96,6 +96,8 @@ class CastModel {
     if (spec === SPEC.RESTODRUID) {
       if (modelID === "Healing Focused") {
         this.modelName = "Healing Focused";
+        this.modelType["Raid"] = "Default"; 
+        this.modelType["Dungeon"] = "Default";
         this.heroTree = "Wildstalker"
         this.runCastModel = scoreDruidSet;
         spellList = druidDefaultSpellData(contentType);
@@ -115,6 +117,8 @@ class CastModel {
     } else if (spec === SPEC.HOLYPALADIN) {
       if (modelID === "Herald of the Sun") {
         this.modelName = "Herald of the Sun";
+        this.modelType["Raid"] = "Default"; 
+        this.modelType["Dungeon"] = "Default";
         spellList = paladinDefaultSpellData(contentType);
         specialQueries = paladinDefaultSpecialQueries(contentType);
         this.baseStatWeights = paladinDefaultStatWeights("Raid");
@@ -128,6 +132,8 @@ class CastModel {
       }
       else if (modelID === "Default") { // Dungeon
         this.modelName = "Default";
+        this.modelType["Raid"] = "Default"; 
+        this.modelType["Dungeon"] = "Default";
         spellList = paladinDefaultSpellData(contentType);
         specialQueries = paladinDefaultSpecialQueries(contentType);
         this.baseStatWeights = paladinDefaultStatWeights(contentType);
@@ -210,6 +216,8 @@ class CastModel {
 
     } else if (spec === SPEC.HOLYPRIEST) {
       this.modelName = "Default";
+      this.modelType["Raid"] = "Default"; 
+      this.modelType["Dungeon"] = "Default";
       spellList = holyPriestDefaultSpellData(contentType);
       specialQueries = holyPriestDefaultSpecialQueries(contentType);
       this.baseStatWeights = holyPriestDefaultStatWeights(contentType);
